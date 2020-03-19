93897ed61aabf97a5a67340fba51409e5f4574b7
Erika Miguel <erika@bocoup.com>
2020-03-19 13:52:28 -0400

    scaffolding for prettier and eslint on client and server, with file formatting after running prettier.

--- server/tests/integration/server.int.test.js
+++ server/tests/integration/server.int.test.js
@@ -1,11 +1,11 @@
-const request = require("supertest");
-const listener = require("../../app");
+const request = require('supertest');
+const listener = require('../../app');
 
-const endpointUrl = "/api";
+const endpointUrl = '/api';
 
 describe(endpointUrl, () => {
     it(`GET ${endpointUrl}`, async () => {
-        const response = await request(listener).get(endpointUrl)
+        const response = await request(listener).get(endpointUrl);
         expect(response.statusCode).toBe(200);
     });
 });
