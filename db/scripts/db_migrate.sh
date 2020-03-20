1c9bec6b869ccbe39a2f7aaae043afb0db1eb223
Erika Miguel <erika@bocoup.com>
2020-03-20 16:02:42 -0400

    db init and flyway scaffolding

---
+++ db/scripts/db_migrate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ENV_FILE=$1
+
+source $ENV_FILE
+
+# Show all the migrations
+flyway info -user=$PGUSER -password=$PGPASSWORD -url=jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE -locations=filesystem:$(pwd)/db/migrations -baselineVersion=0 -baselineOnMigrate=true
+
+# Perform the migration
+flyway migrate -user=$PGUSER -password=$PGPASSWORD -url=jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE -locations=filesystem:$(pwd)/db/migrations -baselineVersion=0 -baselineOnMigrate=true
+
+# Validate the migration
+flyway validate -user=$PGUSER -password=$PGPASSWORD -url=jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE -locations=filesystem:$(pwd)/db/migrations -baselineVersion=0 -baselineOnMigrate=true
---
+++ docs/Database.md
@@ -0,0 +1,24 @@
+# Database setup
+
+## Local database setup
+
+### Dependencies
+* Flyway
+  * Mac Install:
+  ```
+  brew install flyway
+  ```
+
+### Initialize the database
+```
+yarn db-init:dev 
+```
+
+## Run database migrations
+
+### Dev
+```
+yarn db-migrate:dev
+```
+
+
