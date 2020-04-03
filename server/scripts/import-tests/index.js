9e27dc57e6374143308955c23d77c8641a77c32a
Valerie Young <valerie@bocoup.com>
2020-04-02 17:34:25 -0700

    Update script based on expected test refactor

--- server/scripts/import-tests/index.js
+++ server/scripts/import-tests/index.js
@@ -52,23 +52,31 @@ const ariaat = {
             .trim();
         let commit = await nodegit.Commit.lookup(repo, latestCommit);
         let commitDate = commit.date();
+        let commitMsg = commit.message();
 
         const testVersionID = await this.upsertTestVersion(
             ariaAtRepo,
             latestCommit,
-            commitDate
+            commitDate,
+            commitMsg
         );
 
         const support = JSON.parse(fse.readFileSync(supportFile));
-        for (let at of support.ats) {
-            const atID = await this.upsertAt(at.name);
-            await this.upsertAtKey(at.key, atID, testVersionID);
+        const ats = support.ats;
+        for (let at of ats) {
+            const atNameID = await this.upsertAtName(at.name);
+            const atID = await this.upsertAt(at.key, atNameID, testVersionID);
+            at.atID = atID;
         }
 
         const exampleName = {};
         for (let example of support.examples) {
             exampleName[example.directory] = example.name;
         }
+
+        // TODO: When the support.json is updated with information about
+        // which the "applies_to" field in test files, we will need to consume
+        // and use that data
 
         let exampleDirs = fse.readdirSync(testDirectory);
         for (let i = 0; i < exampleDirs.length; i++) {
@@ -99,12 +107,25 @@ const ariaat = {
                         );
                         const testFullName = root.querySelector('title')
                             .innerHTML;
-                        await this.upsertTest(
+                        const testID = await this.upsertTest(
                             testFullName,
                             file,
                             exampleID,
                             testVersionID
                         );
+
+                        // TODO: we need to update this table with the actual mapping between tests
+                        // and screen readers. Right now this data is MOCKED for testing purposes.
+                        for (let at of ats) {
+                            // for a title that includes "switches mode", the test is of mode switching,
+                            // which only occurs for NVDA and JAWS
+                            if (testFullName.indexOf('switches mode') >= 0 && at.key === 'voiceover') continue;
+
+                            await this.upsertTestToAt(
+                                testID,
+                                at.atID
+                            );
+                        }
                     }
                 }
             }
@@ -116,9 +137,10 @@ const ariaat = {
      * @param {string} ariaAtRepo - the repository where these tests exist
      * @param {string} latestCommit - the commit in refs/heads/master at the time of import
      * @param {string} commitDate - the date of import in ISO format
+     * @param {string} commitMsg - the message of the import
      * @return {string} id
      */
-    async upsertTestVersion(ariaAtRepo, latestCommit, commitDate) {
+    async upsertTestVersion(ariaAtRepo, latestCommit, commitDate, commitMsg) {
         const testVersionResult = await client.query(
             'SELECT id FROM test_version WHERE git_repo=$1 and git_hash=$2',
             [ariaAtRepo, latestCommit]
@@ -132,51 +154,52 @@ const ariaat = {
             );
         } else {
             testVersionID = await this.insertRowReturnId(
-                "INSERT INTO test_version(git_repo, git_hash, datetime) VALUES($1, $2, to_date($3, 'YYYY-MM-DDTHH:MI:SS.MSZ')) RETURNING id",
-                [ariaAtRepo, latestCommit, commitDate]
+                "INSERT INTO test_version(git_repo, git_hash, datetime, git_commit_msg) VALUES($1, $2, to_date($3, 'YYYY-MM-DDTHH:MI:SS.MSZ'), $4) RETURNING id",
+                [ariaAtRepo, latestCommit, commitDate, commitMsg]
             );
         }
-        console.log(typeof testVersionID);
         return testVersionID;
     },
 
     /**
-     * Gets at.id and adds the AT record if it does not exist
+     * Gets at_name.id and adds the at_name record if it does not exist
      * @param {string} atName - the human readible name for an AT
      * @return {int} id
      */
-    async upsertAt(atName) {
-        const atResult = await client.query('SELECT id FROM at WHERE name=$1', [
+    async upsertAtName(atName) {
+        const atResult = await client.query('SELECT id FROM at_name WHERE name=$1', [
             atName
         ]);
-        let atID = atResult.rowCount ? atResult.rows[0].id : undefined;
-        if (!atID) {
-            atID = await this.insertRowReturnId(
-                'INSERT INTO at(name) VALUES($1) RETURNING id',
+        let atNameID = atResult.rowCount ? atResult.rows[0].id : undefined;
+        if (!atNameID) {
+            atNameID = await this.insertRowReturnId(
+                'INSERT INTO at_name(name) VALUES($1) RETURNING id',
                 [atName]
             );
         }
-        return atID;
+        return atNameID;
     },
 
     /**
-     * Adds the at_key record if it does not exist
+     * Gets at.id and adds an at record if it does not exist
      * @param {string} atKey - the key for the AT (supplied from the test repo's support.json file)
      * @param {string} atName - the human readible name for an AT
      * @param {int} testVersionID - foreign key into the test_version table
+     * @return {int} id
      */
-    async upsertAtKey(atKey, atID, testVersionID) {
+    async upsertAt(atKey, atNameID, testVersionID) {
         const atKeyResult = await client.query(
-            'SELECT id FROM at_key WHERE key=$1 and test_version_id=$2',
+            'SELECT id FROM at WHERE key=$1 and test_version_id=$2',
             [atKey, testVersionID]
         );
         let atKeyID = atKeyResult.rowCount ? atKeyResult.rows[0].id : undefined;
         if (!atKeyID) {
-            await client.query(
-                'INSERT INTO at_key(key, at_id, test_version_id) VALUES($1, $2, $3) RETURNING id',
-                [atKey, atID, testVersionID]
+            atKeyID = await client.query(
+                'INSERT INTO at(key, at_name_id, test_version_id) VALUES($1, $2, $3) RETURNING id',
+                [atKey, atNameID, testVersionID]
             );
         }
+        return atKeyID;
     },
 
     /**
@@ -204,11 +227,12 @@ const ariaat = {
     },
 
     /**
-     * Adds the test record if it does not exist
+     * Gets the test id and adds the test record if it does not exist
      * @param {string} testFullName - the name of the test
      * @param {string} file - the relative path to the test in the repo
      * @param {int} exampleID - foreign key into the apg_example table
      * @param {int} testVersionID - foreign key into the test_version table
+     * @return {int} id
      */
     async upsertTest(testFullName, file, exampleID, testVersionID) {
         const testResult = await client.query(
@@ -222,7 +246,28 @@ const ariaat = {
                 [testFullName, file, exampleID, testVersionID]
             );
         }
+        return testID;
     },
+
+    /**
+     * Adds the test to at record if it does not exist
+     * @param {int} testID - foreign key into the test table
+     * @param {int} atID - foreign key into the at table
+     */
+    async upsertTestToAt(testID, atID) {
+        const testToAtResult = await client.query(
+            'SELECT id FROM test_to_at WHERE test_id=$1 AND at_id=$2',
+            [testID, atID]
+        );
+        let testToAtID = testToAtResult.rowCount ? testToAtResult.rows[0].id : undefined;
+        if (!testToAtID) {
+            testToAtID = await this.insertRowReturnId(
+                'INSERT INTO test_to_at(test_id, at_id) VALUES($1, $2) RETURNING id',
+                [testID, atID]
+            );
+        }
+    },
+
 
     async insertRowReturnId(query, params) {
         let result;
