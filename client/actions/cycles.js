55b981808f63ec4f216f105889ce765a26e8da87
Valerie Young <valerie@bocoup.com>
2020-04-10 09:15:21 -0700

    dispatch object functions

--- client/actions/cycles.js
+++ client/actions/cycles.js
@@ -1,24 +1,28 @@
 import axios from 'axios';
 import { CYCLES, DELETE_CYCLE } from './types';
 
+export const deleteCycleDispatch = data => ({
+    type: DELETE_CYCLE,
+    payload: data
+});
+
+export const cyclesDispatch = data => ({
+    type: CYCLES,
+    payload: data
+});
+
 export function deleteCycle(id) {
     return async function(dispatch) {
         const response = await axios.delete('/api/cycle', {
             data: { id }
         });
-        dispatch({
-            type: DELETE_CYCLE,
-            payload: response.data
-        });
+        dispatch(deleteCycleDispatch(response.data));
     };
 }
 
 export function getTestCycles() {
     return async function(dispatch) {
         const response = await axios.get('/api/cycle');
-        dispatch({
-            type: CYCLES,
-            payload: response.data
-        });
+        dispatch(cyclesDispatch(response.data));
     };
 }
