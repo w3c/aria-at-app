6e7ba68d3470db4add5b616c7718be5e89d1dee9
Valerie Young <valerie@bocoup.com>
2020-04-10 11:14:35 -0700

    Add sass webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,18 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+              ],
+            },
         ]
     },
     output: {
