bfb899d67fb27b4ed0505a38a9242ed1b85ed202
Rebecca Meritz <rebecca@meritz.com>
2020-05-19 13:54:51 -0700

    Fix the undoing the original migration
    Now `yarn run sequelize -- db:migrate:undo` works.

--- server/migrations/20200518185559-initial-migration.js
+++ server/migrations/20200518185559-initial-migration.js
@@ -16,30 +16,6 @@ module.exports = {
     },
 
     down: queryInterface => {
-        return queryInterface.sequelize.transaction(t => {
-            // TODO: Fix order
-            return Promise.all([
-                queryInterface.dropTable('test_result', { transaction: t }),
-                queryInterface.dropTable('tester_to_run', { transaction: t }),
-                queryInterface.dropTable('user_to_at', { transaction: t }),
-                queryInterface.dropTable('user_to_role', { transaction: t }),
-                queryInterface.dropTable('test_to_at', { transaction: t }),
-                queryInterface.dropTable('test', { transaction: t }),
-                queryInterface.dropTable('apg_example', { transaction: t }),
-                queryInterface.dropTable('at_version', { transaction: t }),
-                queryInterface.dropTable('run', { transaction: t }),
-                queryInterface.dropTable('at', { transaction: t }),
-                queryInterface.dropTable('test_version', { transaction: t }),
-                queryInterface.dropTable('test_cycle', { transaction: t }),
-                queryInterface.dropTable('at_name', { transaction: t }),
-                queryInterface.dropTable('browser_version', { transaction: t }),
-                queryInterface.dropTable('browser', { transaction: t }),
-                queryInterface.dropTable('role', { transaction: t }),
-                queryInterface.dropTable('test_status', { transaction: t }),
-                queryInterface.dropTable('session', { transaction: t }),
-                queryInterface.dropTable('test_version', { transaction: t }),
-                queryInterface.dropTable('users', { transaction: t })
-            ]);
-        });
+        return queryInterface.dropAllTables();
     }
 };
