26517300c2a19512110185766ffd3da33b4cec18
Simon Pieters <zcorpan@gmail.com>
2020-05-20 10:48:30 +0200

    Address comments

--- client/components/Home/index.jsx
+++ client/components/Home/index.jsx
@@ -10,8 +10,15 @@ class Home extends Component {
                     Assistive Technologies (ATs) in how they render ARIA
                     patterns. This is achieved through running manual tests and
                     presenting test results to AT vendors. The tests are based
-                    on examples from WAI-ARIA Authoring Practices, and are
-                    vetted with stakeholders following the working mode process.
+                    {' on examples from '}
+                    <a href="https://w3c.github.io/aria-practices/">
+                        WAI-ARIA Authoring Practices
+                    </a>
+                    , and are vetted with stakeholders following the
+                    <a href="https://github.com/w3c/aria-at/wiki/Working-Mode">
+                        Working Mode
+                    </a>
+                    {' process.'}
                 </p>
 
                 <h2>Get involved</h2>
@@ -20,21 +27,37 @@ class Home extends Component {
                         <a href="https://www.w3.org/community/aria-at/">
                             Join the community group
                         </a>
+                        {' (see '}
+                        <a href="https://www.w3.org/community/about/faq/#how-do-i-join-a-group">
+                            how to join a W3C community group
+                        </a>
+                        )
                     </li>
                     <li>
-                        <a>Sign up as a tester</a>
+                        <a href="https://github.com/w3c/aria-at/issues/162">
+                            Sign up as a tester
+                        </a>
+                        {' for the Pilot Test (May 27 - June 2)'}
                     </li>
                     <li>
-                        <a>Write more tests</a>
+                        <a href="https://github.com/w3c/aria-at/wiki/How-to-contribute-tests">
+                            Write more tests
+                        </a>
                     </li>
                     <li>
-                        <a>Review the assertions of existing test plans</a>
+                        <a href="https://w3c.github.io/aria-at/review-test-plans/">
+                            Review the assertions of existing test plans
+                        </a>
                     </li>
                     <li>
-                        <a>Review test results</a>
+                        <a href="https://w3c.github.io/aria-at/results/">
+                            Review test results
+                        </a>
                     </li>
                     <li>
-                        <a>Fix a good first issue</a>
+                        <a href="https://github.com/w3c/aria-at/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22">
+                            Fix a good first issue
+                        </a>
                     </li>
                 </ul>
             </Fragment>
