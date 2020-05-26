96840dc152df818be9e767a350bdde1e5225741c
Seth Thompson <s3th.thompson@gmail.com>
2020-05-26 14:27:10 -0400

    move aria-at route to app.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const cacheMiddleware = require('apicache').middleware;
+const proxyMiddleware = require('rawgit/lib/middleware');
 const { session } = require('./middleware/session');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const cycleRoutes = require('./routes/cycle');
 const atRoutes = require('./routes/at');
+const path = require('path');
+
+const app = express();
 
 // test session
 app.use(session);
@@ -17,5 +21,18 @@ app.use('/at', atRoutes);
 
 const listener = express();
 listener.use('/api', app);
+
+const BaseURL = 'https://raw.githubusercontent.com';
+const onlyStatus200 = (req, res) => res.statusCode === 200;
+
+listener.route('/aria-at/:branch/*').get(
+    cacheMiddleware('7 days', onlyStatus200),
+    (req, res, next) => {
+        req.url = path.join('w3c', req.url);
+        next();
+    },
+    proxyMiddleware.fileRedirect(BaseURL),
+    proxyMiddleware.proxyPath(BaseURL)
+);
 
 module.exports = { app, listener };
--- server/server.js
+++ server/server.js
@@ -1,23 +1,5 @@
-const path = require('path');
-const cacheMiddleware = require('apicache').middleware;
-const proxyMiddleware = require('rawgit/lib/middleware');
-
-const BaseURL = 'https://raw.githubusercontent.com';
-
 const { listener } = require('./app');
 const port = process.env.PORT || 5000;
-
-const onlyStatus200 = (req, res) => res.statusCode === 200;
-
-listener.route('/aria-at/:branch/*').get(
-    cacheMiddleware('7 days', onlyStatus200),
-    (req, res, next) => {
-        req.url = path.join('w3c', req.url);
-        next();
-    },
-    proxyMiddleware.fileRedirect(BaseURL),
-    proxyMiddleware.proxyPath(BaseURL)
-);
 
 listener.listen(port, () => {
     // eslint-disable-next-line no-console
