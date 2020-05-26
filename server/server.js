ff4903fb7cfe1a89e779db2c8ae7162af811c316
Seth Thompson <s3th.thompson@gmail.com>
2020-05-26 10:27:50 -0400

    consistent naming

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const cache = require('apicache').middleware;
-
+const cacheMiddleware = require('apicache').middleware;
 const proxyMiddleware = require('rawgit/lib/middleware');
+
 const BaseURL = 'https://raw.githubusercontent.com';
 
 const { listener } = require('./app');
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const onlyStatus200 = (req, res) => res.statusCode === 200;
 
 listener.route('/aria-at/:branch/*').get(
-    cache('7 days', onlyStatus200),
+    cacheMiddleware('7 days', onlyStatus200),
     (req, res, next) => {
         req.url = path.join('w3c', req.url);
         next();
