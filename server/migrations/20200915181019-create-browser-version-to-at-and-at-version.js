e073ff02f02793516a60493e718e8222a0015144
Rebecca Meritz <rebecca@meritz.com>
2020-09-15 11:19:01 -0700

    Add BrowserVersionToAtAndAtVersion model
    `npx sequelize-cli model:generate --name BrowserVersionToAtAndAtVersion
    --attributes browser_version_id:integer,at_version_id:integer,at_id:integer,active:boolean`

---
+++ server/migrations/20200915181019-create-browser-version-to-at-and-at-version.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('BrowserVersionToAtAndAtVersions', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            browser_version_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'browser_version',
+                    key: 'id'
+                },
+                allowNull: false
+            },
+            at_version_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'at_version',
+                    key: 'id'
+                },
+                allowNull: false
+            },
+            at_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'at',
+                    key: 'id'
+                },
+                allowNull: false
+            },
+            active: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: false,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: queryInterface => {
+        return queryInterface.dropTable('BrowserVersionToAtAndAtVersions');
+    }
+};
