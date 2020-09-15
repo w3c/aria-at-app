c85f6eb2d7ee9b8ecf8c0d99817be166f1edef49
Rebecca Meritz <rebecca@meritz.com>
2020-09-15 12:28:18 -0700

    Ensure defaults and nulls are create for new table

--- server/models/browserversiontoatandatversion.js
+++ server/models/browserversiontoatandatversion.js
@@ -1,13 +1,41 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const BrowserVersionToAtAndAtVersion = sequelize.define('BrowserVersionToAtAndAtVersion', {
-    browser_version_id: DataTypes.INTEGER,
-    at_version_id: DataTypes.INTEGER,
-    at_id: DataTypes.INTEGER,
-    active: DataTypes.BOOLEAN
-  }, {});
-  BrowserVersionToAtAndAtVersion.associate = function(models) {
-    // associations can be defined here
-  };
-  return BrowserVersionToAtAndAtVersion;
+    const BrowserVersionToAtAndAtVersion = sequelize.define(
+        'BrowserVersionToAtAndAtVersion',
+        {
+            browser_version_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'broswer_version',
+                    key: 'id'
+                }
+            },
+            at_version_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'at_version',
+                    key: 'id'
+                }
+            },
+            at_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'at',
+                    key: 'id'
+                }
+            },
+            active: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            }
+        },
+        {}
+    );
+    BrowserVersionToAtAndAtVersion.associate = function(models) {
+        // associations can be defined here
+    };
+    return BrowserVersionToAtAndAtVersion;
 };
