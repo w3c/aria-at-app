e3d2595d88371ffe927bafa770acc7801c7eb7a1
Rebecca Meritz <rebecca@meritz.com>
2020-09-17 10:03:14 -0700

    Add dbCleaner for writting test in transactions

---
+++ server/tests/util/db-cleaner.js
@@ -0,0 +1,24 @@
+// A method for transactionally cleaning up after unit tests
+// Inspired by the following issue:
+// https://github.com/sequelize/sequelize/issues/11408
+const db = require('../../models/index');
+
+function dbCleaner(fn) {
+    return new Promise((resolve, reject) => {
+        db.sequelize.query('BEGIN;').then(() => {
+            fn()
+                .then(() => {
+                    db.sequelize.query('ROLLBACK;').then(resolve);
+                })
+                .catch(err => {
+                    db.sequelize.query('ROLLBACK;').then(() => {
+                        reject(err);
+                    });
+                });
+        });
+    });
+}
+
+module.exports = {
+    dbCleaner
+};
