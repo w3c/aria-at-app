7cf30e8e2877fcc38b5740270e99d09c105b442e
Erika Miguel <erika@bocoup.com>
2020-10-16 15:09:59 -0400

    Remove components using cycle Redux infrastructure and the components themselves

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import ats from './ats';
-import cycles from './cycles';
 import issues from './issues';
 import runs from './runs';
 import user from './user';
@@ -8,7 +7,6 @@ import users from './users';
 
 export default combineReducers({
     ats,
-    cycles,
     issues,
     runs,
     user,
