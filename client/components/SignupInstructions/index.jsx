4b4715a930b104bee6f841bacf33977f3633bbc4
Erika Miguel <erika@bocoup.com>
2020-11-09 17:29:19 -0500

    Redirect a GitHub user when they are not in an authorized group

--- client/components/SignupInstructions/index.jsx
+++ client/components/SignupInstructions/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SignupInstructions = () => {
-    return <p>This is where signup instructions will go.</p>;
+    return <p>You are not registered with an authorized W3C GitHub team. Please create an issue at <a href="https://github.com/w3c/aria-at-app">ARIA-AT App GitHub</a>.</p>;
 };
 
 export default SignupInstructions;
