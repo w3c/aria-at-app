36fb02e1f91b0189463b470bbe9d25943b3cb2cc
Erika Miguel <erika@bocoup.com>
2020-11-20 15:58:23 -0500

    Adding tester menu

--- client/components/SignupInstructions/index.jsx
+++ client/components/SignupInstructions/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const SignupInstructions = () => {
-    return <p>You are not registered with an authorized W3C GitHub team. Please create an issue at <a href="https://github.com/w3c/aria-at-app">ARIA-AT App GitHub</a>.</p>;
+    return (
+        <p>
+            You are not registered with an authorized W3C GitHub team. Please
+            create an issue at{' '}
+            <a href="https://github.com/w3c/aria-at-app">ARIA-AT App GitHub</a>.
+        </p>
+    );
 };
 
 export default SignupInstructions;
