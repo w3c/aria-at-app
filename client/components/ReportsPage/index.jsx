a19904b63c2f9ef46018e52c8c9072d632dd1177
Erika Miguel <erika@bocoup.com>
2020-11-25 15:55:48 -0500

    Jest tests and refactor for Reports Page

--- client/components/ReportsPage/index.jsx
+++ client/components/ReportsPage/index.jsx
@@ -7,6 +7,11 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolderOpen, faFolder } from '@fortawesome/free-solid-svg-icons';
 import { ProgressBar } from 'react-bootstrap';
+import {
+    generateStateMatrix,
+    calculateTotalObjectPercentage,
+    findAndCalculatePercentage
+} from './utils';
 
 class ReportsPage extends Component {
     constructor() {
@@ -16,6 +21,8 @@ class ReportsPage extends Component {
         };
 
         this.setTechPairsState = this.setTechPairsState.bind(this);
+        this.generateTopLevelData = this.generateTopLevelData.bind(this);
+        this.generateApgExampleRows = this.generateApgExampleRows.bind(this);
     }
 
     componentDidMount() {
@@ -36,70 +43,14 @@ class ReportsPage extends Component {
 
     setTechPairsState() {
         const { publishedRunsById } = this.props;
-
-        this.setState(prevState => {
-            let techMatrix = prevState.techMatrix;
-            Object.values(publishedRunsById).forEach(
-                ({ at_name, browser_name }) => {
-                    // fill in the AT columns
-                    if (!techMatrix[0].includes(at_name)) {
-                        techMatrix[0].push(at_name);
-                    }
-
-                    // Set browser rows
-                    if (!techMatrix.flat().includes(browser_name)) {
-                        techMatrix.push([browser_name]);
-                    }
-                }
-            );
-
-            // Fill the browser roles
-            for (let [i, row] of techMatrix.entries()) {
-                if (row[0] !== null) {
-                    techMatrix[i] = [
-                        row[0],
-                        ...Array(techMatrix[0].length - 1).fill(null)
-                    ];
-                }
-            }
-
-            Object.values(publishedRunsById).forEach(
-                ({ at_name, browser_name, apg_example_name, tests }) => {
-                    const colIdx = techMatrix[0].findIndex(
-                        at => at === at_name
-                    );
-                    const rowIdx = techMatrix.findIndex(
-                        browserRow => browserRow[0] === browser_name
-                    );
-                    const testsWithResults = tests.filter(
-                        test => Object.keys(test.results).length > 0
-                    );
-                    const testsPassed = testsWithResults.filter(
-                        test =>
-                            Object.values(test.results)[0].result.status ===
-                            'PASS'
-                    );
-                    if (techMatrix[rowIdx][colIdx] === null)
-                        techMatrix[rowIdx][colIdx] = {};
-
-                    techMatrix[rowIdx][colIdx][apg_example_name] = {
-                        total: testsWithResults.length,
-                        pass: testsPassed.length
-                    };
-                }
-            );
-
-            return {
-                techMatrix
-            };
-        });
+        let techMatrix = generateStateMatrix(publishedRunsById);
+        this.setState({ techMatrix });
     }
 
-    render() {
-        const { publishedRunsById } = this.props;
+    generateTopLevelData() {
         const { techMatrix } = this.state;
+
         let techPairHeaders = [];
-        let apgExampleRows = [];
         let topLevelRowData = [
             <td key="High level pattern">
                 <span>
@@ -109,159 +60,160 @@ class ReportsPage extends Component {
             </td>
         ];
 
-        if (techMatrix.length > 1) {
-            // Get the table headers by checking with at/browser
-            // row/column is set to true in the techMatrix.
-            // Start at i = 1 because the first row is ats only
-            // Start at j = 1 because the first column is browsers only
-            for (let i = 1; i < techMatrix.length; i++) {
-                for (let j = 1; j < techMatrix[0].length; j++) {
-                    if (techMatrix[i][j] !== null) {
-                        techPairHeaders.push(
-                            <th
-                                key={`${techMatrix[0][j]} with ${techMatrix[i][0]}`}
-                            >
-                                {techMatrix[0][j]} with {techMatrix[i][0]}
-                            </th>
-                        );
-                        const topLevelColumnObj = Object.values(
-                            techMatrix[i][j]
-                        ).reduce(
-                            (acc, { total, pass }) => {
-                                acc.total += total;
-                                acc.pass += pass;
-                                return acc;
-                            },
-                            { total: 0, pass: 0 }
-                        );
+        // Get the table headers by checking which at/browser
+        // row/column is set to an object in the techMatrix.
+        // Start at i = 1 because the first row is ats only
+        // Start at j = 1 because the first column is browsers only
+        for (let i = 1; i < techMatrix.length; i++) {
+            for (let j = 1; j < techMatrix[0].length; j++) {
+                if (techMatrix[i][j] !== null) {
+                    techPairHeaders.push(
+                        <th
+                            key={`${techMatrix[0][j]} with ${techMatrix[i][0]}`}
+                        >
+                            {techMatrix[0][j]} with {techMatrix[i][0]}
+                        </th>
+                    );
 
-                        const percentage = Math.trunc(
-                            (topLevelColumnObj.pass / topLevelColumnObj.total) *
-                                100
-                        );
+                    // Calculate the percentage of this top level column
+                    const percentage = calculateTotalObjectPercentage(
+                        techMatrix[i][j]
+                    );
 
-                        // The math for the top level row works by adding all the passing values
-                        // for each test plan and dividing by the total of all the totals
-                        topLevelRowData.push(
-                            <td
-                                key={`Percentage of ${techMatrix[0][j]} with ${techMatrix[i][0]}`}
-                            >
-                                <ProgressBar
-                                    now={percentage}
-                                    variant="info"
-                                    label={`${percentage}%`}
-                                />
-                            </td>
-                        );
-                    }
+                    // The math for the top level row works by adding all the passing values
+                    // for each test plan and dividing by the total of all the totals
+                    topLevelRowData.push(
+                        <td
+                            key={`Percentage of ${techMatrix[0][j]} with ${techMatrix[i][0]}`}
+                        >
+                            <ProgressBar
+                                now={percentage}
+                                variant="info"
+                                label={`${percentage}%`}
+                            />
+                        </td>
+                    );
                 }
             }
+        }
+        return { techPairHeaders, topLevelRowData };
+    }
 
+    generateApgExampleRows(apgExampleRows, techPairHeaders) {
+        const { techMatrix } = this.state;
+        const { publishedRunsById } = this.props;
+        // Determine which columns should have percentages
+        // for the published result
+        publishedRunsById &&
+            Object.values(publishedRunsById).forEach(run => {
+                const tableRows = [];
+
+                // Get the APG example row index
+                const apgRowIndex = apgExampleRows.findIndex(row =>
+                    row.key.includes(run.apg_example_name)
+                );
+
+                // Get the browser column index
+                const headerColumnIndex = techPairHeaders.findIndex(
+                    header =>
+                        header.key.includes(run.at_name) &&
+                        header.key.includes(run.browser_name)
+                );
+
+                const percentage = findAndCalculatePercentage(
+                    techMatrix,
+                    run.at_name,
+                    run.browser_name,
+                    run.apg_example_name
+                );
+
+                // Add new row
+                if (apgRowIndex < 0) {
+                    tableRows[0] = (
+                        <td key={`example-${run.id}-${run.apg_example_name}`}>
+                            <span>
+                                <FontAwesomeIcon icon={faFolder} />
+                            </span>
+                            {run.apg_example_name}
+                        </td>
+                    );
+                    for (let i = 0; i < techPairHeaders.length; i++) {
+                        if (i === headerColumnIndex) {
+                            tableRows.push(
+                                <td
+                                    key={`data-${run.id}-${run.apg_example_name}-${techPairHeaders[headerColumnIndex].key}`}
+                                >
+                                    <ProgressBar
+                                        now={percentage}
+                                        variant="info"
+                                        label={`${percentage}%`}
+                                    />
+                                </td>
+                            );
+                        } else {
+                            tableRows.push(
+                                <td
+                                    key={`data-${run.id}-${run.apg_example_name}-${techPairHeaders[i].key}`}
+                                >
+                                    -
+                                </td>
+                            );
+                        }
+                    }
+                    apgExampleRows.push(
+                        <tr key={`${run.id}-${run.apg_example_name}`}>
+                            {tableRows}
+                        </tr>
+                    );
+                } else {
+                    // If the row already exists, update the column with percent
+                    let apgExampleRow = apgExampleRows[apgRowIndex];
+                    let dataEntries = [];
+                    for (let dataEntry of apgExampleRow.props.children) {
+                        if (
+                            dataEntry.key.includes(run.at_name) &&
+                            dataEntry.key.includes(run.browser_name)
+                        ) {
+                            dataEntries.push(
+                                <td key={dataEntry.key}>
+                                    <ProgressBar
+                                        now={percentage}
+                                        variant="info"
+                                        label={`${percentage}%`}
+                                    />
+                                </td>
+                            );
+                        } else {
+                            dataEntries.push(dataEntry);
+                        }
+                    }
+                    apgExampleRows[apgRowIndex] = (
+                        <tr key={`${run.id}-${run.apg_example_name}`}>
+                            {dataEntries}
+                        </tr>
+                    );
+                }
+            });
+    }
+
+    render() {
+        const { techMatrix } = this.state;
+        let techPairHeaders = [];
+        let topLevelRowData = [];
+        let apgExampleRows = [];
+
+        // Generate the data table only if there is data
+        if (techMatrix.length > 1) {
+            const topLevelData = this.generateTopLevelData();
+            techPairHeaders = topLevelData.techPairHeaders;
+            topLevelRowData = topLevelData.topLevelRowData;
+
+            // Set the very first row
             apgExampleRows.push(
                 <tr key="ARIA Design Pattern Examples">{topLevelRowData}</tr>
             );
 
-            // Determine which columns should have percentages
-            // for the published result
-            publishedRunsById &&
-                Object.values(publishedRunsById).forEach(run => {
-                    const tableRows = [];
-                    const apgRowIndex = apgExampleRows.findIndex(row =>
-                        row.key.includes(run.apg_example_name)
-                    );
-                    const headerColumnIndex = techPairHeaders.findIndex(
-                        header =>
-                            header.key.includes(run.at_name) &&
-                            header.key.includes(run.browser_name)
-                    );
-                    const techMatrixCol = techMatrix[0].findIndex(
-                        at_name => at_name === run.at_name
-                    );
-                    const techMatrixrow = techMatrix.findIndex(
-                        browserRow => browserRow[0] === run.browser_name
-                    );
-
-                    // The math for the rows works by taking all the passing tests
-                    // and dividing by total number of tests with results
-                    const percentage = Math.trunc(
-                        (techMatrix[techMatrixrow][techMatrixCol][
-                            run.apg_example_name
-                        ].pass /
-                            techMatrix[techMatrixrow][techMatrixCol][
-                                run.apg_example_name
-                            ].total) *
-                            100
-                    );
-
-                    // Add new column
-                    if (apgRowIndex < 0) {
-                        tableRows[0] = (
-                            <td
-                                key={`example-${run.id}-${run.apg_example_name}`}
-                            >
-                                <span>
-                                    <FontAwesomeIcon icon={faFolder} />
-                                </span>
-                                {run.apg_example_name}
-                            </td>
-                        );
-                        for (let i = 0; i < techPairHeaders.length; i++) {
-                            if (i === headerColumnIndex) {
-                                tableRows.push(
-                                    <td
-                                        key={`data-${run.id}-${run.apg_example_name}-${techPairHeaders[headerColumnIndex].key}`}
-                                    >
-                                        <ProgressBar
-                                            now={percentage}
-                                            variant="info"
-                                            label={`${percentage}%`}
-                                        />
-                                    </td>
-                                );
-                            } else {
-                                tableRows.push(
-                                    <td
-                                        key={`data-${run.id}-${run.apg_example_name}-${techPairHeaders[i].key}`}
-                                    >
-                                        -
-                                    </td>
-                                );
-                            }
-                        }
-                        apgExampleRows.push(
-                            <tr key={`${run.id}-${run.apg_example_name}`}>
-                                {tableRows}
-                            </tr>
-                        );
-                    } else {
-                        // If the row already exists, update the column with percent
-                        let apgExampleRow = apgExampleRows[apgRowIndex];
-                        let dataEntries = [];
-                        for (let dataEntry of apgExampleRow.props.children) {
-                            if (
-                                dataEntry.key.includes(run.at_name) &&
-                                dataEntry.key.includes(run.browser_name)
-                            ) {
-                                dataEntries.push(
-                                    <td key={dataEntry.key}>
-                                        <ProgressBar
-                                            now={percentage}
-                                            variant="info"
-                                            label={`${percentage}%`}
-                                        />
-                                    </td>
-                                );
-                            } else {
-                                dataEntries.push(dataEntry);
-                            }
-                        }
-                        apgExampleRows[apgRowIndex] = (
-                            <tr key={`${run.id}-${run.apg_example_name}`}>
-                                {dataEntries}
-                            </tr>
-                        );
-                    }
-                });
+            this.generateApgExampleRows(apgExampleRows, techPairHeaders);
         }
 
         return (
