62230fb9113fa963af7f00cc15b3c8d69c55e17e
alflennik <alflennik@users.noreply.github.com>
2021-06-14 10:40:47 -0400

    PR polish

--- server/tests/util/db-cleaner.js
+++ server/tests/util/db-cleaner.js
@@ -4,8 +4,6 @@ const db = require('../../models/index');
  * Uses a global transaction to clean up the database after each test. All
  * functions which work with the database must check for the
  * global.globalTestTransaction.
- *
- * Inspired by the issue https://github.com/sequelize/sequelize/issues/11408
  * @param {function} callback - async function
  * @returns {*}
  */
@@ -13,11 +11,9 @@ const dbCleaner = async callback => {
     global.globalTestTransaction = await db.sequelize.transaction();
     try {
         await callback();
-        // await db.sequelize.query('ROLLBACK;');
         await global.globalTestTransaction.rollback();
         global.globalTestTransaction = undefined;
     } catch (error) {
-        // await db.sequelize.query('ROLLBACK;');
         await global.globalTestTransaction.rollback();
         global.globalTestTransaction = undefined;
         throw error;
