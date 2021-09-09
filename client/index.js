9c8dd39bf196d27545c0e5b3ff37aff813b980d0
Howard Edwards <howarde.edwards@gmail.com>
2021-09-09 13:50:01 -0400

    start to redux removal

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Store from './store';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 // Order matters for the following two imports
@@ -15,11 +16,13 @@ const client = new ApolloClient({
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Provider store={store}>
+        {/*<Provider store={store}>*/}
+        <Store>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
-        </Provider>
+        </Store>
+        {/*</Provider>*/}
     </ApolloProvider>,
     document.getElementById('root')
 );
---
+++ client/store/reducer.js
@@ -0,0 +1,38 @@
+import { authState, SIGN_IN, SIGN_OUT, SIGN_IN_FAIL } from './auth';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case SIGN_IN: {
+            return {
+                ...state,
+                ...action.payload,
+                isSignInFailed: false,
+                isSignOutCalled: false,
+                isSignedIn: !!action.payload.username,
+                isAdmin: action.payload.roles.includes('ADMIN'),
+                isTester: action.payload.roles.includes('TESTER')
+            };
+        }
+        case SIGN_OUT: {
+            return {
+                ...state,
+                ...authState,
+                isSignedIn: false,
+                isSignOutCalled: true
+            };
+        }
+        case SIGN_IN_FAIL: {
+            return {
+                ...state,
+                ...authState,
+                isSignedIn: false,
+                isSignInFailed: true,
+                isSignOutCalled: true
+            };
+        }
+        default:
+            return state;
+    }
+};
+
+export default reducer;
