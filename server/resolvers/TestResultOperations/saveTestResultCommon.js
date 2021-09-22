229d8946cab86a31c917cf06bf787fc1ec02d978
alflennik <alflennik@users.noreply.github.com>
2021-09-22 10:58:24 -0400

    Address edge case

--- server/resolvers/TestResultOperations/saveTestResultCommon.js
+++ server/resolvers/TestResultOperations/saveTestResultCommon.js
@@ -63,6 +63,8 @@ const saveTestResultCommon = async ({
     if (isSubmit) {
         assertTestResultIsValid(newTestResult);
         newTestResult.completedAt = new Date();
+    } else {
+        newTestResult.completedAt = null;
     }
 
     const index = oldTestResults.findIndex(each => each.id === testResultId);
