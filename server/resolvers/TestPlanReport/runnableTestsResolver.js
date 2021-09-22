cf701c4c0fef004ac635f401478c9c917f0f8ab5
alflennik <alflennik@users.noreply.github.com>
2021-09-22 13:57:08 -0400

    Fix unresolved tests in TestPlanReport

--- server/resolvers/TestPlanReport/runnableTestsResolver.js
+++ server/resolvers/TestPlanReport/runnableTestsResolver.js
@@ -1,8 +1,9 @@
+const testsResolver = require('../TestPlanVersion/testsResolver');
+
 const runnableTestsResolver = testPlanReport => {
-    const {
-        testPlanTarget,
-        testPlanVersion: { tests }
-    } = testPlanReport;
+    const { testPlanTarget, testPlanVersion } = testPlanReport;
+
+    const tests = testsResolver(testPlanVersion);
 
     return tests.filter(test => test.atIds.includes(testPlanTarget.at.id));
 };
