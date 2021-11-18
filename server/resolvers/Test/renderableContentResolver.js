06dfedfa330ddb46039b907c1d0ad03ff6102748
alflennik <alflennik@users.noreply.github.com>
2021-11-18 09:03:26 -0500

    Improve an error message

--- server/resolvers/Test/renderableContentResolver.js
+++ server/resolvers/Test/renderableContentResolver.js
@@ -4,7 +4,8 @@ const renderableContent = (test, { atId }) => {
     if (!atId && !test.inferredAtId) {
         throw new UserInputError(
             'although the atId is optional when it can be inferred from ' +
-                'context, it cannot be inferred in this case.'
+                'context, it cannot be inferred in this case and must be ' +
+                'explicitly provided.'
         );
     }
     return test.renderableContent[atId ?? test.inferredAtId];
