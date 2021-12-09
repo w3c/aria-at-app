dca28cdaaaaa870feead69b836de6b515ce91ca1
Job van Achterberg <mail@3ode.nl>
2021-12-09 13:17:09 +0100

    Use non-breaking-space separator between "Testing task:" span and title within page h1.

--- client/components/TestRun/index.jsx
+++ client/components/TestRun/index.jsx
@@ -583,7 +583,8 @@ const TestRun = () => {
         return (
             <>
                 <h1 ref={titleRef} data-test="testing-task" tabIndex={-1}>
-                    <span className="task-label">Testing task:</span>{' '}
+                    <span className="task-label">Testing task:</span>
+                    <>&nbsp;</>
                     {`${currentTest.seq}.`} {currentTest.title}
                 </h1>
                 <span>{heading}</span>
