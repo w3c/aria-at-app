6016584b740d76a44b4c0a656b5af8ca82ca69f9
Job van Achterberg <mail@3ode.nl>
2021-12-09 15:52:07 +0100

    Implement Test Navigator Toggle button as Disclosure Widget.

--- client/components/TestRun/TestNavigator.jsx
+++ client/components/TestRun/TestNavigator.jsx
@@ -19,13 +19,14 @@ const TestNavigator = ({
     return (
         <Col className="test-navigator" md={show ? 3 : 12}>
             <div className="test-navigator-toggle-container">
-                <div className="test-navigator-toggle-inner-container">
+                <h2
+                    id="test-navigator-heading"
+                    className="test-navigator-toggle-inner-container"
+                >
                     <button
-                        aria-label={`${
-                            show
-                                ? 'Toggle button to close test navigator'
-                                : 'Toggle button to open test navigator'
-                        }`}
+                        aria-label="Test Navigator"
+                        aria-controls="test-navigator-nav"
+                        aria-expanded={show ? 'true' : 'false'}
                         onClick={toggleShowClick}
                         className={`test-navigator-toggle ${
                             show ? 'hide' : 'show'
@@ -38,12 +39,14 @@ const TestNavigator = ({
                         )}
                         <FontAwesomeIcon icon={faAlignLeft} />
                     </button>
-                </div>
-                {show && <h2>Test Navigator</h2>}
+                </h2>
             </div>
             {show && (
-                <nav role="complementary">
-                    <ol className="test-navigator-list">
+                <nav id="test-navigator-nav">
+                    <ol
+                        aria-labelledby="test-navigator-heading"
+                        className="test-navigator-list"
+                    >
                         {tests.map(test => {
                             let resultClassName = 'not-started';
                             let resultStatus = 'Not Started';
