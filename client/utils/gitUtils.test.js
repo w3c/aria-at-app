098bba981e284b92894d0acf8cdb5b6fb66238b0
Job van Achterberg <mail@3ode.nl>
2022-02-02 20:26:24 +0100

    Fix User Locale Date and Time Rendering Edge Case. (#400)

--- client/utils/gitUtils.test.js
+++ client/utils/gitUtils.test.js
@@ -4,12 +4,25 @@ describe('gitUpdatedDateToString', () => {
     it('returns a formatted string AM', () => {
         const date = '2021-11-30T09:51:28.000Z';
         const formattedDate = gitUpdatedDateToString(date);
-        expect(formattedDate).toBe('Nov 30, 2021 at 9:51:28 am');
+        expect(formattedDate).toBe('Nov 30, 2021 at 9:51:28 am UTC');
     });
 
     it('returns a formatted string PM', () => {
         const date = '2021-11-30T14:51:28.000Z';
         const formattedDate = gitUpdatedDateToString(date);
-        expect(formattedDate).toBe('Nov 30, 2021 at 2:51:28 pm');
+        expect(formattedDate).toBe('Nov 30, 2021 at 2:51:28 pm UTC');
+    });
+
+    it('returns a formatted string when using 24-hour notation in a different locale (Dutch)', () => {
+        const date = '2021-11-30T14:51:28.000Z';
+        const formattedDate = gitUpdatedDateToString(date, 'nl-NL');
+        // Dutch time uses a lowercase period-terminated month notation
+        expect(formattedDate).toBe('nov. 30, 2021 at 14:51:28 UTC');
+    });
+
+    it('returns a formatted string when using a different locale (Korean)', () => {
+        const date = '2021-11-30T14:51:28.000Z';
+        const formattedDate = gitUpdatedDateToString(date, 'ko-KR');
+        expect(formattedDate).toBe('11월 30, 2021 at 오후 2:51:28 UTC');
     });
 });
