52555dbd14b1fa5bcc8bc0e31e5be405b9dea1f9
Erika Miguel <erika@bocoup.com>
2022-02-15 17:01:47 -0500

    Populating tool with updater data

--- client/components/TestPlanVersionUpdater/TestPlanVersionUpdater.jsx
+++ client/components/TestPlanVersionUpdater/TestPlanVersionUpdater.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useQuery } from '@apollo/client';
+import { UPDATER_QUERY } from './queries';
+import PageStatus from '../common/PageStatus';
+import { gitUpdatedDateToString } from '../../utils/gitUtils';
 
 const TestPlanVersionUpdater = () => {
+    const { loading, data: updaterData } = useQuery(UPDATER_QUERY);
+
+    if (loading) {
+        return (
+            <PageStatus
+                title="Loading - Version Updater | ARIA-AT"
+                heading="Test Plan Version Updater"
+            />
+        );
+    }
+
+    const {
+        testPlanReport: {
+            testPlanTarget: {
+                at: { name: atName },
+                atVersion,
+                browser: { name: browserName },
+                browserVersion
+            },
+            testPlanVersion: { title }
+        },
+        testPlan: { testPlanVersions }
+    } = updaterData;
+
     return (
         <Container id="main" as="main" tabIndex="-1">
             <h1>Test Plan Version Updater</h1>
@@ -11,16 +39,19 @@ const TestPlanVersionUpdater = () => {
             </p>
             <h2>Selected Report</h2>
             <p>
-                JAWS 1 with Chrome 1 Disclosure Navigation Menu Example
-                Published
+                {`${atName} ${atVersion} with ${browserName} ${browserVersion} ${title}`}
             </p>
             <h2>Pick a Version</h2>
             <p>Current version is from Nov 18, 2021 at 11:38:39 pm UTC</p>
             <select>
-                <option>
-                    Nov 18, 2021 at 11:38:39 pm UTC Metric Updates (#578)
-                    (da10a20)
-                </option>
+                {(() =>
+                    testPlanVersions.map(testPlanVersion => (
+                        <option key={testPlanVersion.id}>
+                            {gitUpdatedDateToString(testPlanVersion.updatedAt)}{' '}
+                            {testPlanVersion.gitMessage} (
+                            {testPlanVersion.gitSha.substring(0, 7)})
+                        </option>
+                    )))()}
             </select>
             <h2>Create the New Report</h2>
             <p>1 test result will be deleted. 33 tests will be copied.</p>
