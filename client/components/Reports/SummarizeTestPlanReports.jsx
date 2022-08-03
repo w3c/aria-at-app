5eda174a7cbdd4269519a15b86d6b42b9382debe
Howard Edwards <howarde.edwards@gmail.com>
2022-08-03 15:36:53 -0500

    Add new Candidate Tests apge

--- client/components/Reports/SummarizeTestPlanReports.jsx
+++ client/components/Reports/SummarizeTestPlanReports.jsx
@@ -63,6 +63,10 @@ const SummarizeTestPlanReports = ({ testPlanReports }) => {
         tabularReports[testPlanVersion.id][testPlanTarget.id] = testPlanReport;
     });
 
+    console.log('testPlanReportsById', testPlanReportsById)
+    console.log('testPlanTargetsById', testPlanTargetsById)
+    console.log('testPlanVersionsById', testPlanVersionsById)
+
     return (
         <FullHeightContainer id="main" as="main" tabIndex="-1">
             <Helmet>
