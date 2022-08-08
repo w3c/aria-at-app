1f27bf1c538900b71d90b37b90e719feab65ae12
Erika Miguel <erika@bocoup.com>
2022-08-08 15:02:01 -0400

    Upgrade storybook

--- client/stories/index.stories.jsx
+++ client/stories/index.stories.jsx
@@ -1,35 +1,14 @@
 import React from 'react';
+import ThankYouModal from '../components/common/CandidateModals/ThankYouModal';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+export default {
+    component: ThankYouModal,
+    title: 'ThankYouModal'
+};
 
-import { Button, Welcome } from '@storybook/react/demo';
-import App from '../components/App';
-import Login from '../components/Login';
-import Signup from '../components/Signup';
+const Template = args => <ThankYouModal {...args} />;
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')} />
-));
-
-storiesOf('Button', module)
-    .add('with text', () => (
-        <Button onClick={action('clicked')}>Hello Button</Button>
-    ))
-    .add('with some emoji', () => (
-        <Button onClick={action('clicked')}>
-            <span role="img" aria-label="so cool">
-                😀 😎 👍 💯
-            </span>
-        </Button>
-    ));
-
-storiesOf('App', module).add('the app', () => <App />);
-storiesOf('Login', module).add('login button', () => (
-    <Login githubClientId={'12345'} />
-));
-
-storiesOf('Signup', module).add('signup button', () => (
-    <Signup onClick={action('clicked')} />
-));
+export const Default = Template.bind({});
+Default.args = {
+    show: true
+};
