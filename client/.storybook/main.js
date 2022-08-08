b99dd773ac1fc3de4064b269357337c4bd4be9ac
Erika Miguel <erika@bocoup.com>
2022-08-08 15:02:01 -0400

    Upgrade storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,3 +1,3 @@
 module.exports = {
-    addons: ['@storybook/addon-actions/register']
+    stories: ['../stories/index.stories.jsx']
 };
---
+++ client/.storybook/preview.js
@@ -0,0 +1,3 @@
+import '!style-loader!css-loader!../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '!style-loader!css-loader!sass-loader!../scss/custom.scss';
+import '../static/index.css';
