36d7887ced96884b02e15acccdc7536a10ff52ad
Howard Edwards <howarde.edwards@gmail.com>
2022-08-24 09:00:09 -0500

    Recursive function for getting all issues through GitHub API

--- server/services/GithubService.js
+++ server/services/GithubService.js
@@ -21,9 +21,119 @@ const permissionScopes = [
     'read:org'
 ];
 const permissionScopesURI = encodeURI(permissionScopes.join(' '));
+const graphQLEndpoint = `${GITHUB_GRAPHQL_SERVER}/graphql`;
+
+const constructIssuesQuery = async ({ ats, after, githubAccessToken }) => {
+    let resultsByAt = { jaws: [], nvda: [], vo: [] };
+
+    const query = `
+        query {
+            repository(owner: "howard-e", name: "aria-at-app") {
+                id
+                issues(
+                  first: 100
+                  ${after ? `after: "${after}"` : ''}
+                  labels: ["app", "candidate-review"]
+                  orderBy: {field: CREATED_AT, direction: DESC}
+                ) {
+                    totalCount
+                    nodes {
+                        body
+                        author {
+                            login
+                        }
+                        closed
+                        title
+                        labels(first: 10) {
+                            totalCount
+                            nodes {
+                                name
+                            }
+                        }
+                        state
+                        number
+                    }
+                    pageInfo {
+                        hasNextPage
+                        endCursor
+                    }
+                }
+            }
+        }
+    `;
+
+    const response = await axios.post(
+        graphQLEndpoint,
+        { query },
+        { headers: { Authorization: `bearer ${githubAccessToken}` } }
+    );
+    const result = response?.data?.data;
+
+    const { repository } = result;
+    const { issues } = repository;
+    const { totalCount, nodes, pageInfo } = issues;
+    const { hasNextPage, endCursor } = pageInfo;
+
+    if (totalCount) {
+        resultsByAt = {
+            jaws: ats.includes('jaws')
+                ? [
+                      ...resultsByAt.jaws,
+                      ...nodes.filter(issue =>
+                          issue.labels.nodes
+                              .map(label => label.name)
+                              .includes('jaws')
+                      )
+                  ]
+                : [],
+            nvda: ats.includes('nvda')
+                ? [
+                      ...resultsByAt.nvda,
+                      ...nodes.filter(issue =>
+                          issue.labels.nodes
+                              .map(label => label.name)
+                              .includes('nvda')
+                      )
+                  ]
+                : [],
+            vo: ats.includes('vo')
+                ? [
+                      ...resultsByAt.vo,
+                      ...nodes.filter(issue =>
+                          issue.labels.nodes
+                              .map(label => label.name)
+                              .includes('vo')
+                      )
+                  ]
+                : []
+        };
+    }
+
+    if (hasNextPage) {
+        // call next page and build the resultsByAt again
+        const additionalResultsByAt = await constructIssuesQuery({
+            ats,
+            after: endCursor,
+            githubAccessToken
+        });
+        resultsByAt = {
+            jaws: ats.includes('jaws')
+                ? [...resultsByAt.jaws, ...additionalResultsByAt.jaws]
+                : [],
+            nvda: ats.includes('nvda')
+                ? [...resultsByAt.nvda, ...additionalResultsByAt.nvda]
+                : [],
+            vo: ats.includes('vo')
+                ? [...resultsByAt.vo, ...additionalResultsByAt.vo]
+                : []
+        };
+    }
+
+    return resultsByAt;
+};
 
 module.exports = {
-    graphQLEndpoint: `${GITHUB_GRAPHQL_SERVER}/graphql`,
+    graphQLEndpoint,
     getOauthUrl: ({ state = '' }) => {
         return (
             `${GITHUB_OAUTH_SERVER}/login/oauth/authorize?scope=` +
@@ -93,5 +203,11 @@ module.exports = {
             .find(teamName => teamName === GITHUB_TEAM_ADMIN);
 
         return isMember;
+    },
+    async getCandidateReviewIssues({
+        ats = ['jaws', 'nvda', 'vo'],
+        githubAccessToken
+    }) {
+        return await constructIssuesQuery({ ats, githubAccessToken });
     }
 };
