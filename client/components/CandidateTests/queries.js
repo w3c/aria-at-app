8e9f93736d13d43b1b018a94dd20b8f479082b03
Howard Edwards <howarde.edwards@gmail.com>
2022-10-04 16:34:29 -0500

    Use pre-computed metrics for candidate tests page

--- client/components/CandidateTests/queries.js
+++ client/components/CandidateTests/queries.js
@@ -4,6 +4,7 @@ export const CANDIDATE_TESTS_PAGE_QUERY = gql`
     query {
         testPlanReports(statuses: [IN_REVIEW]) {
             id
+            metrics
             at {
                 id
                 name
@@ -20,84 +21,6 @@ export const CANDIDATE_TESTS_PAGE_QUERY = gql`
                     directory
                 }
                 metadata
-            }
-            runnableTests {
-                id
-                title
-                renderedUrl
-            }
-            finalizedTestResults {
-                id
-                test {
-                    id
-                    rowNumber
-                    title
-                    renderedUrl
-                }
-                scenarioResults {
-                    id
-                    scenario {
-                        commands {
-                            id
-                            text
-                        }
-                    }
-                    output
-                    assertionResults {
-                        id
-                        assertion {
-                            text
-                        }
-                        passed
-                        failedReason
-                    }
-                    requiredAssertionResults: assertionResults(
-                        priority: REQUIRED
-                    ) {
-                        assertion {
-                            text
-                        }
-                        passed
-                        failedReason
-                    }
-                    optionalAssertionResults: assertionResults(
-                        priority: OPTIONAL
-                    ) {
-                        assertion {
-                            text
-                        }
-                        passed
-                        failedReason
-                    }
-                    unexpectedBehaviors {
-                        id
-                        text
-                        otherUnexpectedBehaviorText
-                    }
-                }
-            }
-            draftTestPlanRuns {
-                tester {
-                    username
-                }
-                testPlanReport {
-                    id
-                    status
-                }
-                testResults {
-                    test {
-                        id
-                    }
-                    atVersion {
-                        id
-                        name
-                    }
-                    browserVersion {
-                        id
-                        name
-                    }
-                    completedAt
-                }
             }
             # vendorReviewStatus
             candidateStatusReachedAt
