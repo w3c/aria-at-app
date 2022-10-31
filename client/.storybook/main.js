8ec077375344616154e30f45d21a658e37a42354
Erika Miguel <erika@bocoup.com>
2022-10-31 11:17:44 -0400

    Revert storybook configs

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
     stories: ['../stories/*.stories.jsx'],
-    addons: [
-        '@storybook/addon-a11y',
-        '@storybook/addon-controls',
-        'storybook-addon-apollo-client'
-    ]
+    addons: ['@storybook/addon-a11y', '@storybook/addon-controls']
 };
--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,11 +2,3 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/@fortawesome/fontawesome-svg-core/styles.css';
 import '!style-loader!css-loader!sass-loader!../scss/custom.scss'; // using inline webpack loaders for scss
 import '../static/index.css';
-import { MockedProvider } from '@apollo/client/testing';
-export const parameters = {
-    apolloClient: {
-        MockedProvider,
-        // any props you want to pass to MockedProvider on every story
-        addTypename: false
-    }
-};
