10b784cf21037ed20a44d64535d9963432d23c10
Howard Edwards <howarde.edwards@gmail.com>
2023-01-09 20:40:47 -0500

    jest.setup.js update

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,5 +1,3 @@
 const fetch = require('node-fetch');
-const ResizeObserver = require('resize-observer-polyfill');
 
 global.fetch = fetch;
-global.ResizeObserver = require('resize-observer-polyfill')
