d791f17146707433b077821a7a759c7177487d81
Howard Edwards <howarde.edwards@gmail.com>
2023-01-24 09:12:48 -0500

    Revert "Update React"
    This reverts commit d09df2fc468d59598ebd3bdb54a56ee65936860c.

--- client/components/ConfirmAuth/index.jsx
+++ client/components/ConfirmAuth/index.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useQuery } from '@apollo/client';
 import { ME_QUERY } from '../App/queries';
 import { evaluateAuth } from '../../utils/evaluateAuth';
 
-const ConfirmAuth = ({ children, requiredPermission }) => {
+const ConfirmAuth = ({ children, requiredPermission, ...rest }) => {
     const { data } = useQuery(ME_QUERY);
 
     const auth = evaluateAuth(data && data.me ? data.me : {});
     const { roles, username, isAdmin, isSignedIn } = auth;
 
-    if (!username) return <Navigate to="/invalid-request" />;
+    if (!username) return <Redirect to={{ pathname: '/invalid-request' }} />;
 
     // If you are an admin, you can access all other role actions by default
     const authConfirmed =
         isSignedIn && (roles.includes(requiredPermission) || isAdmin);
 
-    if (!authConfirmed) return <Navigate to="/404" />;
-
-    return children;
+    return (
+        <Route
+            {...rest}
+            render={() => {
+                return authConfirmed ? (
+                    children
+                ) : (
+                    <Redirect to={{ pathname: '/404' }} />
+                );
+            }}
+        />
+    );
 };
 
 ConfirmAuth.propTypes = {
