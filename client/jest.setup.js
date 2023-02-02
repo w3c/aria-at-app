1e2f8668380f9c4a6b10d596e3c5a1e8bdc6b7b2
Erika Miguel <erika@bocoup.com>
2023-02-02 15:45:34 -0500

    adding text encoder, which is not added to jsdom

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,7 +1,5 @@
 const fetch = require('node-fetch');
 const textEncoder = require('util').TextEncoder;
-// const ResizeObserver = require('resize-observer-polyfill');
 
 global.fetch = fetch;
 global.TextEncoder = textEncoder;
-// global.ResizeObserver = require('resize-observer-polyfill')
