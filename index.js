50b47433706db0243143a10c29fed638c6472f56
Erika Miguel <erika@bocoup.com>
2023-02-13 15:43:39 -0500

    Vagrant environment changes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const history = require('connect-history-api-fallback');
 const path = require('path');
 
 const { listener } = require('./server/server');
-listener.use(express.static('./client/dist'));
+// Need to use a fallback api so that the server
+// can redirect to the frontend routes
+listener.use(history()).use(express.static('./client/dist'));
 
 listener.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/dist/index.html'));
