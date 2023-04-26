8b5387fbf68060e9e3a1c95ec5cec2a9f84b38c2
Howard Edwards <howarde.edwards@gmail.com>
2023-04-26 16:43:50 -0500

    Address test failure

--- server/resolvers/testPlanVersionsResolver.js
+++ server/resolvers/testPlanVersionsResolver.js
@@ -9,6 +9,7 @@ const { TEST_PLAN_VERSION_ATTRIBUTES } = require('../models/services/helpers');
 
 const testPlanVersionsResolver = async (root, args, context, info) => {
     const where = {};
+    const directoryNamesObject = {};
 
     const { attributes: testPlanVersionAttributes } = retrieveAttributes(
         'testPlanVersion',
@@ -16,28 +17,32 @@ const testPlanVersionsResolver = async (root, args, context, info) => {
         info
     );
 
-    const directoryNames = fs
-        .readFileSync(
-            path.resolve(
-                __dirname,
-                '../scripts/import-tests/directory_names.txt'
+    const isDirectoryIncluded = testPlanVersionAttributes.includes('directory');
+    if (isDirectoryIncluded) {
+        // This can only be verified `testPlan.directory` is also being referenced
+        // But should we force `directory` to be a part of testPlanVersion queries?
+        const directoryNames = fs
+            .readFileSync(
+                path.resolve(
+                    __dirname,
+                    '../scripts/import-tests/directory_names.txt'
+                )
             )
-        )
-        .toString();
+            .toString();
 
-    const directoryObject = {};
-    const directoryNamesSplit = directoryNames.trim().split('\n');
-    directoryNamesSplit.forEach(i => {
-        const directoryNameSplit = i.split(','); // eg. 'alert,Alert Example'
-        const directory = directoryNameSplit[0]; // eg. 'alert'
-        directoryObject[directory] = directoryNameSplit[1]; // Alert Example;
-    });
+        const directoryNamesSplit = directoryNames.trim().split('\n');
+        directoryNamesSplit.forEach(i => {
+            const directoryNameSplit = i.split(','); // eg. 'alert,Alert Example'
+            const directory = directoryNameSplit[0]; // eg. 'alert'
+            directoryNamesObject[directory] = directoryNameSplit[1]; // Alert Example;
+        });
 
-    const directories = Object.keys(directoryObject);
+        const directories = Object.keys(directoryNamesObject);
 
-    // If there is a directory which is not included in the most recently updated version of
-    // `directory_names.txt`, it will not be shown.
-    if (directories.length) where.directory = directories;
+        // If there is a directory which is not included in the most recently updated version of
+        // `directory_names.txt`, it will not be shown.
+        if (directories.length) where.directory = directories;
+    }
 
     const testPlanVersions = await getTestPlanVersions(
         null,
@@ -59,16 +64,18 @@ const testPlanVersionsResolver = async (root, args, context, info) => {
         }
     );
 
-    // Check if there are any invalid titles before mutating result object.
-    // So any without a title or a title which doesn't fall into `Object.values(directoryObject)
-    const isTitlesValid = await isInvalidDirectoryOrTitles({
-        titles: Object.values(directoryObject)
-    });
-
-    if (!isTitlesValid) {
-        testPlanVersions.forEach(t => {
-            t.title = directoryObject[t.directory];
+    if (isDirectoryIncluded) {
+        // Check if there are any invalid titles before mutating result object.
+        // Any without a title, or a title which doesn't fall into `Object.values(directoryNamesObject)
+        const isTitlesValid = await isInvalidDirectoryOrTitles({
+            titles: Object.values(directoryNamesObject)
         });
+
+        if (!isTitlesValid) {
+            testPlanVersions.forEach(t => {
+                t.title = directoryNamesObject[t.directory];
+            });
+        }
     }
 
     return testPlanVersions;
