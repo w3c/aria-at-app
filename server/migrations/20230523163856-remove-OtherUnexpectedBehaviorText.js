4a15c7b738bdb43c1d914031ee0189ef11cab7b9
Howard Edwards <howarde.edwards@gmail.com>
2023-06-07 10:17:44 -0500

    Revise updateTestPlanRun blocks

--- server/migrations/20230523163856-remove-OtherUnexpectedBehaviorText.js
+++ server/migrations/20230523163856-remove-OtherUnexpectedBehaviorText.js
@@ -5,61 +5,81 @@ const conflictsResolver = require('../resolvers/TestPlanReport/conflictsResolver
 module.exports = {
     up: queryInterface => {
         return queryInterface.sequelize.transaction(async transaction => {
-            const resultsQuery = await queryInterface.sequelize.query(
+            const testPlanRunQuery = await queryInterface.sequelize.query(
                 `SELECT id, "testResults" FROM "TestPlanRun"`,
                 {
                     transaction
                 }
             );
 
-            const results = resultsQuery[0];
-            if (!results) {
+            const testPlanReportQuery = await queryInterface.sequelize.query(
+                `SELECT id FROM "TestPlanReport"`,
+                {
+                    transaction
+                }
+            );
+
+            const testPlanRunData = testPlanRunQuery[0];
+            if (!testPlanRunData) {
                 // eslint-disable-next-line no-console
                 console.info('The test Results are empty');
                 return;
             }
-            let testPlanRunId;
-            for (let i = 0; i < results.length; i += 1) {
-                let needsUpdate = false;
-                let updateParams = {};
-                testPlanRunId = results[i].id;
 
-                if (!results[i].testResults) {
+            for (let i = 0; i < testPlanRunData.length; i += 1) {
+                const testPlanRun = testPlanRunData[i];
+                const testPlanRunId = testPlanRun.id;
+                let needsUpdate = false;
+
+                let updateParams = {
+                    testResults: testPlanRun.testResults
+                };
+
+                // eslint-disable-next-line no-console
+                console.info(
+                    `=== Id for TestPlanRun results:${testPlanRunId} ===`
+                );
+
+                if (!testPlanRunData[i].testResults) {
                     continue;
                 }
                 if (
-                    Array.isArray(results[i].testResults) &&
-                    results[i].testResults.length < 1
+                    Array.isArray(testPlanRunData[i].testResults) &&
+                    testPlanRunData[i].testResults.length < 1
                 ) {
                     continue;
                 }
 
-                for (let j = 0; j < results[i].testResults.length; j += 1) {
-                    // eslint-disable-next-line no-console
-                    console.info(
-                        `=== Id for TestPlanRun results:${testPlanRunId} ===`
-                    );
-                    if (!results[i].testResults[j].scenarioResults) {
+                for (
+                    let j = 0;
+                    j < testPlanRunData[i].testResults.length;
+                    j += 1
+                ) {
+                    if (!testPlanRunData[i].testResults[j].scenarioResults) {
                         console.log(7);
                         continue;
                     }
                     if (
                         Array.isArray(
-                            results[i].testResults[j].scenarioResults
+                            testPlanRunData[i].testResults[j].scenarioResults
                         ) &&
-                        results[i].testResults[j].scenarioResults.length < 1
+                        testPlanRunData[i].testResults[j].scenarioResults
+                            .length < 1
                     ) {
                         console.log(8);
                         continue;
                     }
                     for (
                         let p = 0;
-                        p < results[i].testResults[j].scenarioResults.length;
+                        p <
+                        testPlanRunData[i].testResults[j].scenarioResults
+                            .length;
                         p += 1
                     ) {
                         if (
-                            !results[i].testResults[j].scenarioResults[p]
-                                .unexpectedBehaviors
+                            !testPlanRunData[i].testResults[j].scenarioResults[
+                                p
+                            ].unexpectedBehaviors
                         ) {
                             console.log(9);
                             continue;
@@ -67,10 +87,10 @@ module.exports = {
 
                         if (
                             Array.isArray(
-                                results[i].testResults[j].scenarioResults[p]
-                                    .unexpectedBehaviors
+                                testPlanRunData[i].testResults[j]
+                                    .scenarioResults[p].unexpectedBehaviors
                             ) &&
-                            results[i].testResults[j].scenarioResults[p]
+                            testPlanRunData[i].testResults[j].scenarioResults[p]
                                 .unexpectedBehaviors.length < 1
                         ) {
                             console.log(10);
@@ -80,7 +100,7 @@ module.exports = {
                         for (
                             let s = 0;
                             s <
-                            results[i].testResults[j].scenarioResults[p]
+                            testPlanRunData[i].testResults[j].scenarioResults[p]
                                 .unexpectedBehaviors.length;
                             s += 1
                         ) {
@@ -89,8 +109,8 @@ module.exports = {
                             // }
                             console.log(11);
                             const unexpectedBehavior =
-                                results[i].testResults[j].scenarioResults[p]
-                                    .unexpectedBehaviors[s];
+                                testPlanRunData[i].testResults[j]
+                                    .scenarioResults[p].unexpectedBehaviors[s];
                             if (
                                 unexpectedBehavior.id !== 'OTHER' &&
                                 unexpectedBehavior.otherUnexpectedBehaviorText ===
@@ -99,23 +119,27 @@ module.exports = {
                                 console.log(12);
                                 delete unexpectedBehavior.otherUnexpectedBehaviorText;
                                 console.log(13);
+
+                                updateParams.testResults[j] =
+                                    testPlanRunData[i].testResults[j];
+
+                                needsUpdate = true;
                             }
-                            results[i].testResults[j].scenarioResults[
-                                p
-                            ].unexpectedBehaviors[s] = unexpectedBehavior;
-                            needsUpdate = true;
                         }
                         console.timeEnd('Line 110');
                     }
-                    updateParams = {
-                        testResults: results[i].testResults
-                    };
+                    // updateParams = {
+                    //     testResults: testPlanRunData[i].testResults
+                    // };
                     // console.log("PARAMS", JSON.stringify(updateParams, null, 2));
                     // console.log("PARAMS", updateParams, JSON.stringify(updateParams?.testResults?.scenarioResults, updateParams?.testResults?.scenarioResults, null, 2));
-                    if (needsUpdate)
-                        await updateTestPlanRun(testPlanRunId, updateParams);
                 }
+
+                if (needsUpdate)
+                    await updateTestPlanRun(testPlanRunId, updateParams);
             }
+
+            // TODO: Iterate over testPlanReports to calculate conflictCount in metrics
         });
     }
 };
