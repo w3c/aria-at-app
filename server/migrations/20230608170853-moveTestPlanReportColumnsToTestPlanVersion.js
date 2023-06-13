80aa906910e93f537c57b7ca55d0a7b04ffaba58
Howard Edwards <howarde.edwards@gmail.com>
2023-06-13 15:39:14 -0500

    Add phase to TestPlanVersion

--- server/migrations/20230608170853-moveTestPlanReportColumnsToTestPlanVersion.js
+++ server/migrations/20230608170853-moveTestPlanReportColumnsToTestPlanVersion.js
@@ -6,6 +6,17 @@ module.exports = {
         return queryInterface.sequelize.transaction(async transaction => {
             await queryInterface.addColumn(
                 'TestPlanVersion',
+                'phase',
+                {
+                    type: Sequelize.DataTypes.TEXT,
+                    allowNull: false,
+                    defaultValue: 'DRAFT'
+                },
+                { transaction }
+            );
+
+            await queryInterface.addColumn(
+                'TestPlanVersion',
                 'candidateStatusReachedAt',
                 {
                     type: Sequelize.DataTypes.DATE,
@@ -41,6 +52,9 @@ module.exports = {
 
     async down(queryInterface) {
         return queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.removeColumn('TestPlanVersion', 'phase', {
+                transaction
+            });
             await queryInterface.removeColumn(
                 'TestPlanVersion',
                 'candidateStatusReachedAt',
