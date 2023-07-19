7288c7afdf37a68e9a13b19d22f1ee0d5d3a8e89
Howard Edwards <howarde.edwards@gmail.com>
2023-07-19 18:48:37 -0500

    Add approvedAt column to TestPlanReport

--- server/migrations/20230614004831-removeDateColumnsFromTestPlanReport.js
+++ server/migrations/20230614004831-removeDateColumnsFromTestPlanReport.js
@@ -2,8 +2,28 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface) {
+    async up(queryInterface, Sequelize) {
         return queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.addColumn(
+                'TestPlanReport',
+                'approvedAt',
+                {
+                    type: Sequelize.DataTypes.DATE,
+                    defaultValue: null,
+                    allowNull: true
+                },
+                { transaction }
+            );
+
+            await queryInterface.sequelize.query(
+                `UPDATE "TestPlanReport"
+                        SET "approvedAt" = "candidateStatusReachedAt"
+                        WHERE "candidateStatusReachedAt" IS NOT NULL`,
+                {
+                    transaction
+                }
+            );
+
             await queryInterface.removeColumn(
                 'TestPlanReport',
                 'candidateStatusReachedAt',
@@ -30,6 +50,10 @@ module.exports = {
 
     async down(queryInterface, Sequelize) {
         return queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.removeColumn('TestPlanReport', 'approvedAt', {
+                transaction
+            });
+
             await queryInterface.addColumn(
                 'TestPlanReport',
                 'candidateStatusReachedAt',
