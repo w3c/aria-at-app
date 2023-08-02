6f5e8c8e3bfec5c4be669d3d00775f000ec6e57d
Stalgia Grigg <stalgia@bocoup.com>
2023-08-02 17:45:36 -0400

    Refactor test and test for correct mutation call

--- client/tests/AddTestToQueueWithConfirmation.test.jsx
+++ client/tests/AddTestToQueueWithConfirmation.test.jsx
@@ -6,17 +6,29 @@ import React from 'react';
 import { render, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MockedProvider } from '@apollo/client/testing';
+
 // eslint-disable-next-line jest/no-mocks-import
 import { TEST_QUEUE_MUTATION_MOCK } from './__mocks__/GraphQLMocks';
 import AddTestToQueueWithConfirmation from '../components/AddTestToQueueWithConfirmation';
 import { BrowserRouter } from 'react-router-dom';
-import { InMemoryCache } from '@apollo/client';
+import { InMemoryCache, useMutation } from '@apollo/client';
 
-const setup = props => {
+jest.mock('@apollo/client');
+
+let mutationMock;
+let mockTestPlanVersion,
+    mockBrowser,
+    mockAt,
+    mockButtonText,
+    getByTestId,
+    findByRole;
+
+const setup = (props, mockMutation) => {
+    useMutation.mockReturnValue([mockMutation, {}]);
     return render(
         <BrowserRouter>
             <MockedProvider
-                mocks={TEST_QUEUE_MUTATION_MOCK}
+                mocks={[]}
                 cache={new InMemoryCache({ addTypename: false })}
             >
                 <AddTestToQueueWithConfirmation {...props} />
@@ -24,58 +36,66 @@ const setup = props => {
         </BrowserRouter>
     );
 };
-//eslint-disable-next-line
-describe('AddTestToQueueWithConfirmation', () => {
-    const mockTestPlanVersion = {
-        id: 5
-    };
-    const mockBrowser = {
-        id: 2
-    };
-    const mockAt = {
-        id: 3
-    };
-    let mockButtonText = 'Add to Test Queue';
 
-    test('renders Button without error', async () => {
-        const { getByTestId } = setup({
+const commonSetup = mockMutation => {
+    mockTestPlanVersion = { id: 5 };
+    mockBrowser = { id: 2 };
+    mockAt = { id: 3 };
+    mockButtonText = 'Add to Test Queue';
+
+    const renderResult = setup(
+        {
             testPlanVersion: mockTestPlanVersion,
             browser: mockBrowser,
             at: mockAt,
             buttonText: mockButtonText
-        });
+        },
+        mockMutation
+    );
 
+    getByTestId = renderResult.getByTestId;
+    findByRole = renderResult.findByRole;
+};
+
+describe('AddTestToQueueWithConfirmation', () => {
+    beforeEach(() => {
+        mutationMock = jest.fn().mockResolvedValue(TEST_QUEUE_MUTATION_MOCK);
+        commonSetup(mutationMock);
+    });
+
+    test('renders Button without error', async () => {
         await waitFor(() =>
             expect(getByTestId('add-button')).toBeInTheDocument()
         );
     });
 
     test('Button has correct text', async () => {
-        const { getByTestId } = setup({
-            testPlanVersion: mockTestPlanVersion,
-            browser: mockBrowser,
-            at: mockAt,
-            buttonText: mockButtonText
-        });
-
         await waitFor(() =>
             expect(getByTestId('add-button')).toHaveTextContent(mockButtonText)
         );
     });
 
     test('renders BasicModal without error', async () => {
-        const { findByRole, getByTestId } = setup({
-            testPlanVersion: mockTestPlanVersion,
-            browser: mockBrowser,
-            at: mockAt,
-            buttonText: mockButtonText
-        });
-
         fireEvent.click(getByTestId('add-button'));
 
         await waitFor(async () => {
             const modal = await findByRole('dialog');
             expect(modal).toBeInTheDocument();
+        });
+    });
+
+    test('calls mutation on button click with correct variables', async () => {
+        fireEvent.click(getByTestId('add-button'));
+
+        await waitFor(() => {
+            expect(mutationMock).toHaveBeenCalled();
+            expect(mutationMock).toHaveBeenCalledWith({
+                variables: {
+                    testPlanVersionId: mockTestPlanVersion.id,
+                    atId: mockAt.id,
+                    browserId: mockBrowser.id
+                }
+            });
         });
     });
 });
