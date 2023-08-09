adba33bce2eac78e93ce801bb86e8b70b02540cf
Howard Edwards <howarde.edwards@gmail.com>
2023-08-09 14:33:11 -0400

    Add check for conflicts when marking report as final

--- server/resolvers/TestPlanReportOperations/markAsFinalResolver.js
+++ server/resolvers/TestPlanReportOperations/markAsFinalResolver.js
@@ -5,6 +5,7 @@ const {
 } = require('../../models/services/TestPlanReportService');
 const finalizedTestResultsResolver = require('../TestPlanReport/finalizedTestResultsResolver');
 const populateData = require('../../services/PopulatedData/populateData');
+const conflictsResolver = require('../TestPlanReport/conflictsResolver');
 
 const markAsFinalResolver = async (
     { parentContext: { id: testPlanReportId } },
@@ -19,14 +20,18 @@ const markAsFinalResolver = async (
 
     const testPlanReport = await getTestPlanReportById(testPlanReportId);
 
+    const conflicts = await conflictsResolver(testPlanReport, null, context);
+    if (conflicts.length > 0) {
+        throw new Error(
+            'Cannot mark test plan report as final due to conflicts'
+        );
+    }
+
     const finalizedTestResults = await finalizedTestResultsResolver(
-        {
-            ...testPlanReport
-        },
+        testPlanReport,
         null,
         context
     );
-
     if (!finalizedTestResults || !finalizedTestResults.length) {
         throw new Error(
             'Cannot mark test plan report as final because there are no ' +
