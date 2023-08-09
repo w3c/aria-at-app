23df30069980dfa700234c914296ea042008713e
Howard Edwards <howarde.edwards@gmail.com>
2023-08-09 14:17:33 -0400

    Update markAsFinalResolver.js to check if report can be finalized

--- server/resolvers/TestPlanReportOperations/markAsFinalResolver.js
+++ server/resolvers/TestPlanReportOperations/markAsFinalResolver.js
@@ -1,7 +1,9 @@
 const { AuthenticationError } = require('apollo-server');
 const {
+    getTestPlanReportById,
     updateTestPlanReport
 } = require('../../models/services/TestPlanReportService');
+const finalizedTestResultsResolver = require('../TestPlanReport/finalizedTestResultsResolver');
 const populateData = require('../../services/PopulatedData/populateData');
 
 const markAsFinalResolver = async (
@@ -13,6 +15,23 @@ const markAsFinalResolver = async (
 
     if (!user?.roles.find(role => role.name === 'ADMIN')) {
         throw new AuthenticationError();
+    }
+
+    const testPlanReport = await getTestPlanReportById(testPlanReportId);
+
+    const finalizedTestResults = await finalizedTestResultsResolver(
+        {
+            ...testPlanReport
+        },
+        null,
+        context
+    );
+
+    if (!finalizedTestResults || !finalizedTestResults.length) {
+        throw new Error(
+            'Cannot mark test plan report as final because there are no ' +
+                'completed test results'
+        );
     }
 
     await updateTestPlanReport(testPlanReportId, { approvedAt: new Date() });
