7de6d95348042b417123eb3dea425e98c2ec7680
Stalgia Grigg <stalgia@bocoup.com>
2023-08-17 12:11:53 -0400

    Functioning sort

--- client/components/common/SortableTableHeader/index.js
+++ client/components/common/SortableTableHeader/index.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faArrowDownShortWide,
+    faArrowUpShortWide
+} from '@fortawesome/free-solid-svg-icons';
+import { TABLE_SORT_ORDERS } from '../../../utils/enums';
 
 const SortableTableHeaderWrapper = styled.th`
     position: relative;
@@ -21,9 +27,12 @@ const SortableTableHeaderButton = styled(Button)`
     bottom: 0;
     left: 0;
     right: 0;
-    padding: 32px 12px;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 8px 12px;
     border-radius: 0;
     z-index: 0;
+    display: flex;
     &:hover,
     &:focus {
         background: #e9ebee;
@@ -37,22 +46,37 @@ const SortableTableHeaderButton = styled(Button)`
     }
 `;
 
-export const TABLE_SORT_ORDERS = {
-    ASC: 'ASCENDING',
-    DESC: 'DESCENDING'
-};
+const InactiveIcon = styled(FontAwesomeIcon)`
+    color: rgb(155, 155, 155);
+`;
 
-const SortableTableHeader = ({ title, onSort = () => {} }) => {
-    const [currentSortOrder, setCurrentSortOrder] = useState(null);
+const SortableTableHeader = ({ title, active, onSort = () => {} }) => {
+    const [currentSortOrder, setCurrentSortOrder] = useState(
+        TABLE_SORT_ORDERS.ASC
+    );
 
     const handleClick = () => {
         const newSortOrder =
             currentSortOrder === TABLE_SORT_ORDERS.ASC
                 ? TABLE_SORT_ORDERS.DESC
                 : TABLE_SORT_ORDERS.ASC;
-        console.log(newSortOrder);
         setCurrentSortOrder(newSortOrder);
         onSort(newSortOrder);
+    };
+
+    const getIcon = () => {
+        if (!active) {
+            return <InactiveIcon icon={faArrowDownShortWide} />;
+        }
+
+        switch (currentSortOrder) {
+            case TABLE_SORT_ORDERS.ASC:
+                return <FontAwesomeIcon icon={faArrowUpShortWide} />;
+            case TABLE_SORT_ORDERS.DESC:
+                return <FontAwesomeIcon icon={faArrowDownShortWide} />;
+            default:
+                return <FontAwesomeIcon icon={faArrowDownShortWide} />;
+        }
     };
 
     return (
@@ -62,6 +86,7 @@ const SortableTableHeader = ({ title, onSort = () => {} }) => {
                 aria-label={`Change the table sort order to sort by ${currentSortOrder} based on the ${title} column`}
             >
                 {title}
+                {getIcon()}
             </SortableTableHeaderButton>
         </SortableTableHeaderWrapper>
     );
@@ -69,6 +94,7 @@ const SortableTableHeader = ({ title, onSort = () => {} }) => {
 
 SortableTableHeader.propTypes = {
     title: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
     onSort: PropTypes.func.isRequired
 };
 
