0457ead2e1c0fe99d0f91a5a83a9b8e68088779c
Stalgia Grigg <stalgia@bocoup.com>
2023-08-21 10:30:01 -0600

    unit tests for SortableTableHeader

---
+++ client/tests/SortableTableHeader.test.jsx
@@ -0,0 +1,62 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import SortableTableHeader from '../components/common/SortableTableHeader';
+import { TABLE_SORT_ORDERS } from '../utils/constants';
+import '@testing-library/jest-dom';
+
+const renderComponent = props => render(<SortableTableHeader {...props} />);
+
+const getAriaSort = (active, sortOrder) =>
+    active
+        ? sortOrder === TABLE_SORT_ORDERS.ASC
+            ? 'ascending'
+            : 'descending'
+        : 'none';
+
+describe('SortableTableHeader component', () => {
+    const defaultProps = { title: 'Header', active: false, onSort: () => {} };
+
+    it('should render without crashing', () => {
+        renderComponent(defaultProps);
+        expect(screen.getByRole('columnheader')).toBeInTheDocument();
+    });
+
+    it('should render the correct title', () => {
+        renderComponent(defaultProps);
+        expect(screen.getByText('Header')).toBeInTheDocument();
+    });
+
+    it('should render the inactive icon when active is false', () => {
+        renderComponent(defaultProps);
+        expect(screen.getByRole('img', { hidden: true })).toBeInTheDocument();
+    });
+
+    it('should handle sorting order and aria-sort attribute when active is true and clicked', () => {
+        const onSort = jest.fn();
+        renderComponent({ ...defaultProps, active: true, onSort });
+        const button = screen.getByRole('button');
+
+        expect(screen.getByRole('columnheader')).toHaveAttribute(
+            'aria-sort',
+            getAriaSort(true, TABLE_SORT_ORDERS.ASC)
+        );
+
+        fireEvent.click(button);
+        expect(onSort).toHaveBeenCalledWith(TABLE_SORT_ORDERS.DESC);
+        expect(screen.getByRole('columnheader')).toHaveAttribute(
+            'aria-sort',
+            getAriaSort(true, TABLE_SORT_ORDERS.DESC)
+        );
+
+        fireEvent.click(button);
+        expect(onSort).toHaveBeenCalledWith(TABLE_SORT_ORDERS.ASC);
+        expect(screen.getByRole('columnheader')).toHaveAttribute(
+            'aria-sort',
+            getAriaSort(true, TABLE_SORT_ORDERS.ASC)
+        );
+    });
+});
