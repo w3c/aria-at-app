7df16d8f2680d219770606624827072e2f5c5e1a
Stalgia Grigg <stalgia@bocoup.com>
2023-08-29 16:03:57 -0700

    Unit tests for updating CollectionJob

--- server/tests/integration/automation-scheduler.test.js
+++ server/tests/integration/automation-scheduler.test.js
@@ -21,19 +21,13 @@ afterAll(async () => {
     await db.sequelize.close();
 });
 
-describe('Automation Controller', () => {
+describe('Schedule jobs with automation controller', () => {
     it('should schedule a new job', async () => {
-        const response = await sessionAgent
-            .post('/api/jobs/new')
-            .send({
-                test: 'test'
-            })
-            .set(
-                'x-automation-secret',
-                process.env.AUTOMATION_SCHEDULER_SECRET
-            );
+        const response = await sessionAgent.post('/api/jobs/new').send({
+            test: 'test'
+        });
 
-        // expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({
             id: '999',
             status: 'QUEUED'
@@ -41,13 +35,8 @@ describe('Automation Controller', () => {
     });
 
     it('should cancel a job', async () => {
-        const response = await sessionAgent
-            .post('/api/jobs/999/cancel')
-            .set(
-                'x-automation-secret',
-                process.env.AUTOMATION_SCHEDULER_SECRET
-            );
-        // expect(response.statusCode).toBe(200);
+        const response = await sessionAgent.post('/api/jobs/999/cancel');
+        expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({
             id: '999',
             status: 'CANCELED'
@@ -55,13 +44,8 @@ describe('Automation Controller', () => {
     });
 
     it('should restart a job', async () => {
-        const response = await sessionAgent
-            .post('/api/jobs/999/restart')
-            .set(
-                'x-automation-secret',
-                process.env.AUTOMATION_SCHEDULER_SECRET
-            );
-        // expect(response.statusCode).toBe(200);
+        const response = await sessionAgent.post('/api/jobs/999/restart');
+        expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({
             id: '999',
             status: 'QUEUED'
@@ -69,16 +53,34 @@ describe('Automation Controller', () => {
     });
 
     it('should get a job log', async () => {
+        const response = await sessionAgent.get('/api/jobs/999/log');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+            id: '999',
+            log: 'TEST LOG'
+        });
+    });
+
+    it('should not update a job status without verification', async () => {
+        const response = await sessionAgent.post('/api/jobs/999/update');
+        expect(response.statusCode).toBe(403);
+        expect(response.body).toEqual({
+            error: 'Unauthorized'
+        });
+    });
+
+    it('should update a job status with verification', async () => {
         const response = await sessionAgent
-            .get('/api/jobs/999/log')
+            .post('/api/jobs/999/update')
+            .send({ status: 'RUNNING' })
             .set(
                 'x-automation-secret',
                 process.env.AUTOMATION_SCHEDULER_SECRET
             );
-        // expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({
             id: '999',
-            log: 'TEST LOG'
+            status: 'RUNNING'
         });
     });
 });
