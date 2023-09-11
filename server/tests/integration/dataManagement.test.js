3b9c22cfa0a696bc617257427f31d0253d79636e
Howard Edwards <howarde.edwards@gmail.com>
2023-09-11 10:47:08 -0500

    Extend default timeout

--- server/tests/integration/dataManagement.test.js
+++ server/tests/integration/dataManagement.test.js
@@ -3,6 +3,10 @@ const dbCleaner = require('../util/db-cleaner');
 const { query, mutate } = require('../util/graphql-test-utilities');
 const db = require('../../models');
 
+beforeAll(() => {
+    jest.setTimeout(20000);
+});
+
 afterAll(async () => {
     // Closing the DB connection allows Jest to exit successfully.
     await db.sequelize.close();
