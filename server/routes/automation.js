c6221a762057d0c70352d29aaf3385d9e4014170
Stalgia Grigg <stalgia@bocoup.com>
2023-09-21 08:54:06 -0700

    router.use handleError in automation routes

--- server/routes/automation.js
+++ server/routes/automation.js
@@ -11,20 +11,12 @@ const { handleError } = require('../middleware/handleError');
 
 const router = Router();
 
-router.get('/:jobID/log', getJobLog, handleError);
+router.get('/:jobID/log', getJobLog);
 
-router.post(
-    '/:jobID/update',
-    verifyAutomationScheduler,
-    updateJobStatus,
-    handleError
-);
+router.post('/:jobID/update', verifyAutomationScheduler, updateJobStatus);
 
-router.post(
-    '/:jobID/result',
-    verifyAutomationScheduler,
-    updateJobResults,
-    handleError
-);
+router.post('/:jobID/result', verifyAutomationScheduler, updateJobResults);
+
+router.use(handleError);
 
 module.exports = router;
