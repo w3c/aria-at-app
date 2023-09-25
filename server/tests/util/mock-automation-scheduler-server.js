f7265c590290ae5f4acf725f0f3cc9d5c11b64bc
Stalgia Grigg <stalgia@bocoup.com>
2023-09-25 15:11:39 -0700

    Allow mock automation scheduler to post results

--- server/tests/util/mock-automation-scheduler-server.js
+++ server/tests/util/mock-automation-scheduler-server.js
@@ -4,6 +4,9 @@ const {
     verifyAutomationScheduler
 } = require('../../middleware/verifyAutomationScheduler');
 const { COLLECTION_JOB_STATUS } = require('../../util/enums');
+const { default: axios } = require('axios');
+const { gql } = require('apollo-server-core');
+const { query } = require('../util/graphql-test-utilities');
 
 const setupMockAutomationSchedulerServer = async () => {
     const app = express();
@@ -23,15 +26,156 @@ const setupMockAutomationSchedulerServer = async () => {
         return Math.floor(Math.random() * 1000000).toString();
     };
 
-    app.post('/jobs/new', (req, res) => {
+    const simulateJobStatusUpdate = async (jobId, newStatus) => {
+        await axios.post(
+            `${process.env.APP_SERVER}/api/jobs/${jobId}/update`,
+            {
+                status: newStatus
+            },
+            {
+                headers: {
+                    'x-automation-secret':
+                        process.env.AUTOMATION_SCHEDULER_SECRET
+                },
+                timeout: 1000
+            }
+        );
+    };
+
+    const simulateResultCompletion = async (
+        tests,
+        atVersionName,
+        browserVersionName,
+        jobId,
+        currentTestIndex
+    ) => {
+        const currentTest = tests[currentTestIndex];
+        const { id, scenarios, assertions } = currentTest;
+
+        const responses = [];
+        scenarios.forEach(() => {
+            assertions.forEach(() => {
+                responses.push('Local development simulated output');
+            });
+        });
+        const testResult = {
+            testId: id,
+            atVersionName,
+            browserVersionName,
+            responses
+        };
+        await axios.post(
+            `${process.env.APP_SERVER}/api/jobs/${jobId}/result`,
+            testResult,
+            {
+                headers: {
+                    'x-automation-secret':
+                        process.env.AUTOMATION_SCHEDULER_SECRET
+                },
+                timeout: 1000
+            }
+        );
+
+        if (currentTestIndex < tests.length - 1) {
+            setTimeout(() => {
+                simulateResultCompletion(
+                    tests,
+                    atVersionName,
+                    browserVersionName,
+                    jobId,
+                    currentTestIndex + 1
+                );
+            }, Math.random() * 1000);
+        }
+
+        return testResult;
+    };
+
+    app.post('/jobs/new', async (req, res) => {
         if (process.env.ENVIRONMENT === 'test') {
             return res.json({
                 id: '999',
                 status: COLLECTION_JOB_STATUS.QUEUED
             });
         } else {
+            // Local development must simulate posting results
+            const { testPlanVersionGitSha, testPlanName } = req.body;
+
+            const { testPlanVersions } = await query(gql`
+                query {
+                    testPlanVersions {
+                        id
+                        gitSha
+                        testPlan {
+                            id
+                        }
+                        testPlanReports {
+                            at {
+                                name
+                                atVersions {
+                                    name
+                                }
+                            }
+                            browser {
+                                name
+                                browserVersions {
+                                    name
+                                }
+                            }
+                            runnableTests {
+                                id
+                                scenarios {
+                                    id
+                                }
+                                assertions {
+                                    id
+                                }
+                            }
+                        }
+                    }
+                }
+            `);
+            const testPlanVersion = testPlanVersions.find(
+                testPlanVersion =>
+                    testPlanVersion.gitSha === testPlanVersionGitSha &&
+                    testPlanVersion.testPlan.id === testPlanName
+            );
+
+            const testPlanReport = testPlanVersion.testPlanReports.find(
+                testPlanReport =>
+                    testPlanReport.at.name === 'NVDA' &&
+                    testPlanReport.browser.name === 'Chrome'
+            );
+
+            const browserVersionName =
+                testPlanReport.browser.browserVersions[0].name;
+
+            const atVersionName = testPlanReport.at.atVersions[0].name;
+            const { runnableTests } = testPlanReport;
+            const jobId = generateRandomId();
+
+            setTimeout(
+                () =>
+                    simulateJobStatusUpdate(
+                        jobId,
+                        COLLECTION_JOB_STATUS.RUNNING
+                    ),
+                1000
+            );
+
+            setTimeout(
+                () =>
+                    simulateResultCompletion(
+                        runnableTests,
+                        atVersionName,
+                        browserVersionName,
+                        jobId,
+                        0
+                    ),
+                3000
+            );
             return res.json({
-                id: generateRandomId(),
+                id: jobId,
                 status: COLLECTION_JOB_STATUS.QUEUED
             });
         }
