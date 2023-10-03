c3f5e0f499024255e8d4fbaf85d618de967fcc33
Stalgia Grigg <stalgia@bocoup.com>
2023-10-02 17:11:23 -0700

    Create FinishBotRunDialog.test.jsx

---
+++ client/tests/FinishBotRunDialog.test.jsx
@@ -0,0 +1,99 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import FinishBotRunDialog from '../components/FinishBotRunButtonWithDialog/FinishBotRunDialog';
+import {
+    COLLECTION_JOB_ID_BY_TEST_PLAN_RUN_ID_QUERY,
+    MARK_COLLECTION_JOB_AS_FINISHED
+} from '../components/FinishBotRunButtonWithDialog/queries';
+import '@testing-library/jest-dom/extend-expect';
+
+const mocks = [
+    {
+        request: {
+            query: COLLECTION_JOB_ID_BY_TEST_PLAN_RUN_ID_QUERY,
+            variables: {
+                testPlanRunId: '1'
+            }
+        },
+        result: {
+            data: {
+                collectionJobByTestPlanRunId: {
+                    id: 'collection-job-1'
+                }
+            }
+        }
+    },
+    {
+        request: {
+            query: MARK_COLLECTION_JOB_AS_FINISHED,
+            variables: {
+                collectionJobId: 'collection-job-1'
+            }
+        },
+        result: {
+            data: {
+                collectionJob: {
+                    markCollectionJobFinished: {
+                        id: 'collection-job-1',
+                        status: 'CANCELLED'
+                    }
+                }
+            }
+        }
+    }
+];
+
+describe('FinishBotRunDialog', () => {
+    test('renders without crashing', () => {
+        render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <FinishBotRunDialog
+                    testPlanRun={{ id: '1' }}
+                    show={true}
+                    setShow={jest.fn()}
+                    testers={[]}
+                    testPlanReportId="1"
+                    onChange={jest.fn()}
+                    onDelete={jest.fn()}
+                />
+            </MockedProvider>
+        );
+
+        expect(screen.getByText('Finish Bot Run')).toBeInTheDocument();
+    });
+
+    test('triggers correct actions when buttons are clicked', async () => {
+        const mockOnChange = jest.fn();
+        const mockOnDelete = jest.fn();
+
+        render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <FinishBotRunDialog
+                    testPlanRun={{ id: '1' }}
+                    show={true}
+                    setShow={jest.fn()}
+                    testers={[]}
+                    testPlanReportId="1"
+                    onChange={mockOnChange}
+                    onDelete={mockOnDelete}
+                />
+            </MockedProvider>
+        );
+
+        await waitFor(() => new Promise(resolve => setTimeout(resolve, 0)));
+
+        const markAsFinishedButton = screen.getByText('Mark as finished');
+        const deleteButton = screen.getByLabelText('Delete bot run');
+
+        fireEvent.click(markAsFinishedButton);
+
+        await waitFor(() => expect(mockOnChange).toHaveBeenCalled());
+
+        fireEvent.click(deleteButton);
+        expect(mockOnDelete).toHaveBeenCalled();
+    });
+});
