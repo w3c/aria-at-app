f741c5cb2561be8d06bf98ea93624239dd6d1fce
Stalgia Grigg <stalgia@bocoup.com>
2023-10-05 11:06:31 -0700

    Remove errant useRef

--- client/components/TestQueue/AssignTesterDropdown/index.jsx
+++ client/components/TestQueue/AssignTesterDropdown/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Dropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
