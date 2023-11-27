7146600cd04ef11ee056e7c6d38ced5ada1c73d0
Howard Edwards <howarde.edwards@gmail.com>
2023-11-27 13:48:56 -0500

    Fix bug found after rebase and update jsdoc on 'retrieveAttributes'

--- server/resolvers/helpers/retrieveAttributes.js
+++ server/resolvers/helpers/retrieveAttributes.js
@@ -2,12 +2,12 @@ const deriveAttributesFromCustomField = require('./deriveAttributesFromCustomFie
 
 const getAttributesFromSelections = (
     selections,
-    modelName = null,
+    fieldName = null,
     useSubpath = false
 ) => {
     return (
         useSubpath
-            ? selections.filter(item => item.name.value === modelName)
+            ? selections.filter(item => item.name.value === fieldName)
             : selections
     ).map(({ name: { value }, selectionSet }) => {
         return {
@@ -20,15 +20,25 @@ const getAttributesFromSelections = (
     });
 };
 
+/**
+ * Used to return the ONLY database attributes a query may require to process a graphql query
+ * @param {string} fieldName - The referenced field name defined in graphql-schema. eg. testPlanReport, testPlanVersion,
+ *        draftTestPlanRuns
+ * @param {string[]} modelAttributes - The 'known' database attributes the field could include
+ * @param {Array<FieldNode>} fieldNodes - Apollo GraphQL's fieldNodes included in a query's info object
+ *        {see https://www.apollographql.com/docs/apollo-server/data/resolvers/#resolver-arguments}
+ * @param {boolean} useSubpath - Flag to check attributes at increased depths of the query object
+ * @returns {{raw: *[], attributes: any[]}}
+ */
 const retrieveAttributes = (
-    modelName,
+    fieldName,
     modelAttributes,
     { fieldNodes },
     useSubpath = false
 ) => {
     const attributes = getAttributesFromSelections(
         fieldNodes[0].selectionSet.selections,
-        modelName,
+        fieldName,
         useSubpath
     );
 
@@ -44,7 +54,7 @@ const retrieveAttributes = (
     });
 
     const { fields, derived } = deriveAttributesFromCustomField(
-        modelName,
+        fieldName,
         unknown
     );
 
