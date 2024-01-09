ca03ee257e27ac755e3dee7fc3323fb59ea01325
Howard Edwards <howarde.edwards@gmail.com>
2024-01-09 13:32:38 -0500

    Remove includeImpactCalculation param from calculateAssertionsCount func

--- client/components/common/TestPlanResultsTable/utils.js
+++ client/components/common/TestPlanResultsTable/utils.js
@@ -1,7 +1,4 @@
-export const calculateAssertionsCount = (
-    testResult,
-    includeImpactCalculation = true
-) => {
+export const calculateAssertionsCount = testResult => {
     let passedAssertionsCount = testResult.scenarioResults.reduce(
         (acc, scenarioResult) =>
             acc + scenarioResult.assertionResults.filter(e => e.passed).length,
@@ -14,29 +11,26 @@ export const calculateAssertionsCount = (
         0
     );
 
-    if (includeImpactCalculation) {
-        // For each command, a check for high negative and moderate negative impacts
-        const maxUnexpectedBehaviorsPassCount =
-            testResult.scenarioResults.length * 2;
+    // For each command, a check for high negative and moderate negative impacts, so 2
+    const maxUnexpectedBehaviorsPassCount =
+        testResult.scenarioResults.length * 2;
 
-        // These are equivalent to failures
-        const unexpectedBehaviorCount = testResult.scenarioResults.reduce(
-            (acc, scenarioResult) =>
-                acc +
-                (scenarioResult.unexpectedBehaviors.some(
-                    e => e.severity === 'HIGH' || e.severity === 'MODERATE'
-                )
-                    ? 1
-                    : 0),
-            0
-        );
+    // These are equivalent to failures
+    const unexpectedBehaviorCount = testResult.scenarioResults.reduce(
+        (acc, scenarioResult) =>
+            acc +
+            (scenarioResult.unexpectedBehaviors.some(
+                e => e.severity === 'HIGH' || e.severity === 'MODERATE'
+            )
+                ? 1
+                : 0),
+        0
+    );
 
-        const additionalPassedAssertions =
-            maxUnexpectedBehaviorsPassCount - unexpectedBehaviorCount;
-        passedAssertionsCount =
-            passedAssertionsCount + additionalPassedAssertions;
-        failedAssertionsCount = failedAssertionsCount + unexpectedBehaviorCount;
-    }
+    const additionalPassedAssertions =
+        maxUnexpectedBehaviorsPassCount - unexpectedBehaviorCount;
+    passedAssertionsCount = passedAssertionsCount + additionalPassedAssertions;
+    failedAssertionsCount = failedAssertionsCount + unexpectedBehaviorCount;
 
     return { passedAssertionsCount, failedAssertionsCount };
 };
