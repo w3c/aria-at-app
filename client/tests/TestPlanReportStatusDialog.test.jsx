b94f5f6bbe552aff1a623894b077f969f424bbbe
Stalgia Grigg <stalgia@bocoup.com>
2024-01-29 15:39:25 -0800

    Fix warnings in TestPlanReportStatusDialog

--- client/tests/TestPlanReportStatusDialog.test.jsx
+++ client/tests/TestPlanReportStatusDialog.test.jsx
@@ -14,8 +14,51 @@ import TestPlanReportStatusDialog from '../components/TestPlanReportStatusDialog
 import { TEST_PLAN_REPORT_STATUS_DIALOG_MOCK_DATA } from './__mocks__/GraphQLMocks';
 // eslint-disable-next-line jest/no-mocks-import
 import { mockedTestPlanVersion } from './__mocks__/GraphQLMocks/TestPlanReportStatusDialogMock';
+import { TEST_PLAN_RUN_REPORTS_INITIATED_BY_AUTOMATION } from '../components/AddTestToQueueWithConfirmation/queries';
+import { ME_QUERY } from '../components/App/queries';
 
-const setup = (props, mocks = []) => {
+const mocks = [
+    {
+        request: {
+            query: TEST_PLAN_RUN_REPORTS_INITIATED_BY_AUTOMATION,
+            variables: {
+                testPlanVersionId: '7'
+            }
+        },
+        result: {
+            data: {
+                testPlanVersion: {
+                    id: '7',
+                    testPlanReports: [
+                        {
+                            id: '1',
+                            markedFinalAt: '2021-01-01T00:00:00.000Z',
+                            draftTestPlanRuns: {
+                                initiatedByAutomation: true
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    {
+        request: {
+            query: ME_QUERY
+        },
+        result: {
+            data: {
+                me: {
+                    id: '1',
+                    username: 'testUser',
+                    roles: ['ADMIN']
+                }
+            }
+        }
+    }
+];
+
+const setup = props => {
     return render(
         <BrowserRouter>
             <MockedProvider
