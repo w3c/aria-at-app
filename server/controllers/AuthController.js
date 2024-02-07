0043ffd39596307efb937c0256fbf647fc374ce2
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-02-07 16:41:25 -0500

    Add amins file and update getUsers function

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -43,25 +43,26 @@ const oauthRedirectFromGithubController = async (req, res) => {
 
     if (!githubUsername) return loginFailedDueToGitHub();
 
-    const isAdmin = await GithubService.isMemberOfAdminTeam({
-        githubAccessToken,
-        githubUsername
-    });
-    if (isAdmin == null) return loginFailedDueToGitHub();
+    // const isAdmin = await GithubService.isMemberOfAdminTeam({
+    //     githubAccessToken,
+    //     githubUsername
+    // });
+    // if (isAdmin == null) return loginFailedDueToGitHub();
 
-    const testers = await getUsersFromFile('../../testers.txt');
-    const vendors = await getUsersFromFile('../../vendors.txt');
+    const admins = await getUsersFromFile('admins.txt');
+    const testers = await getUsersFromFile('testers.txt');
+    const vendors = await getUsersFromFile('vendors.txt');
 
     const roles = [];
-    if (isAdmin) {
+    if (admins.includes(githubUsername)) {
         roles.push({ name: User.ADMIN });
     }
-    if (isAdmin || testers.includes(githubUsername)) {
+    if (admins.includes(githubUsername) || testers.includes(githubUsername)) {
         roles.push({ name: User.TESTER }); // Admins are always testers
     }
 
     if (
-        isAdmin ||
+        admins.includes(githubUsername) ||
         vendors.findIndex(vendor => vendor.includes(githubUsername)) > -1
     ) {
         roles.push({ name: User.VENDOR });
--- server/util/getUsersFromFile.js
+++ server/util/getUsersFromFile.js
@@ -1,9 +1,13 @@
-const path = require('path');
-const fs = require('fs/promises');
+// const path = require('path');
+// const fs = require('fs/promises');
 
 const getUsersFromFile = async file => {
-    const roleGroupPath = path.join(__dirname, file);
-    const roleGroupTxt = await fs.readFile(roleGroupPath, { encoding: 'utf8' });
+    // const roleGroupPath = path.join(__dirname, file);
+    // const roleGroupTxt = await fs.readFile(roleGroupPath, { encoding: 'utf8' });
+    const response = await fetch(
+        `https://raw.githubusercontent.com/w3c/aria-at-app/main/${file}`
+    );
+    const roleGroupTxt = await response.text();
     const linesRaw = roleGroupTxt.split('\n');
     const noComments = linesRaw.filter(line => line.substr(0, 1) !== '#');
     const noEmpties = noComments.filter(line => line.trim().length > 0);
@@ -11,4 +15,16 @@ const getUsersFromFile = async file => {
     return testers;
 };
 
+/* TODO:
+    ✅1) Make the function accept just "vendors.txt" or "testers.txt" instead of a full path
+    
+    2) Figure out how to test this
+        --HINT: Change line 8 to be on a branch that is easier to change
+
+    3) Add "admins.txt" file. Account for the fact that everyone is not an admin
+     in production -- developers.
+
+    4) Remove all code related to github teams 
+*/
+getUsersFromFile('vendors').then(console.log);
 module.exports = getUsersFromFile;
