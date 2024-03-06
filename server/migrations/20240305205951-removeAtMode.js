a980d7df6c45b60356b0798d364594bcc0f45448
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-03-06 15:31:08 -0500

    Remove instance of atMode in tests column

--- server/migrations/20240305205951-removeAtMode.js
+++ server/migrations/20240305205951-removeAtMode.js
@@ -2,12 +2,34 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface /* , Sequelize */) {
+    async up(queryInterface, Sequelize) {
         return queryInterface.sequelize.transaction(async transaction => {
             await queryInterface.dropTable('AtMode', {
                 cascade: true,
                 transaction
             });
+
+            const testPlanVersions = await queryInterface.sequelize.query(
+                `SELECT id, tests FROM "TestPlanVersion"`,
+                {
+                    type: Sequelize.QueryTypes.SELECT,
+                    transaction
+                }
+            );
+            await Promise.all(
+                testPlanVersions.map(async ({ id, tests }) => {
+                    const updatedTests = JSON.stringify(
+                        tests.map(test => {
+                            delete test.atMode;
+                            return test;
+                        })
+                    );
+                    await queryInterface.sequelize.query(
+                        `UPDATE "TestPlanVersion" SET tests = ? WHERE id = ?`,
+                        { replacements: [updatedTests, id], transaction }
+                    );
+                })
+            );
         });
     }
 };
