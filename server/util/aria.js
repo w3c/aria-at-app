63a6db17012abff0e8f7a7c89b1a281a4d09bf30
Howard Edwards <howarde.edwards@gmail.com>
2024-03-11 16:48:51 -0500

    Support copying of results' assertions even when there is a changed assertion (#956)
    * Start tracking assertionId from assertions.csv inside TestPlanVersion.tests[].assertions to make determining if an assertion needs to be updated easier
    
    * Update hashing function to not account for assertions during update process
    
    * Update updatePhaseResolver.js to account for making lower assertion-level comparisons instead of a broad test-level comparison which invalidates an entire test if even a single assertion is changed
    
    * Remove commented out lines
    
    * Remove comment
    
    * Updates
    
    * Confirm length of assertions array across versions
    
    * Noticed an issue which could prevent tests from being saved on when navigating between tests during last pass

--- server/util/aria.js
+++ server/util/aria.js
@@ -7,25 +7,49 @@ const evaluateAtNameKey = atName => {
     else return atName.toLowerCase();
 };
 
-const testWithOmittedAttributes = test => ({
-    ...omit(test, [
+const testWithModifiedAttributes = (test, { forUpdateCompare }) => {
+    let propertiesToOmit = [
         'id',
         'at',
         'renderedUrls',
         'renderedUrl',
         'renderableContent.target.at.raw',
         'viewers'
-    ]),
-    assertions: test.assertions.map(assertion => omit(assertion, ['id'])),
-    scenarios: test.scenarios.map(scenario => omit(scenario, ['id']))
-});
+    ];
+
+    // During comparison for phase update, we only need to make sure the assertionId hasn't
+    // changed so the potentially copied result isn't affected by the assertionStatement or
+    // assertionPhrase being changed
+    if (forUpdateCompare) {
+        // Changed text in renderableContent.assertions[].assertion(Statement|Phrase) shouldn't
+        // matter during comparison of results
+        propertiesToOmit.push('renderableContent.assertions');
+
+        // The collection of assertions won't matter during an update comparison; a per-assertion
+        // check will that is handled separately in processCopiedReports.js
+        propertiesToOmit.push('assertions');
+
+        return {
+            ...omit(test, propertiesToOmit),
+            scenarios: test.scenarios.map(scenario => omit(scenario, ['id']))
+        };
+    } else {
+        return {
+            ...omit(test, propertiesToOmit),
+            assertions: test.assertions.map(assertion =>
+                omit(assertion, ['id'])
+            ),
+            scenarios: test.scenarios.map(scenario => omit(scenario, ['id']))
+        };
+    }
+};
 
 // Ideally the hash of tests being imported will never change
-const hashTests = tests => objectHash(tests.map(testWithOmittedAttributes));
+const hashTests = tests => objectHash(tests.map(testWithModifiedAttributes));
 
 // Generate the hash of a test.
-const hashTest = test => {
-    return objectHash(testWithOmittedAttributes(test));
+const hashTest = (test, { forUpdateCompare = false } = {}) => {
+    return objectHash(testWithModifiedAttributes(test, { forUpdateCompare }));
 };
 
 module.exports = {
