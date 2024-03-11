e0d7c511b024a4e52cf2b74b86afd22560b96e5e
Howard Edwards <howarde.edwards@gmail.com>
2024-03-11 13:14:07 -0500

    Update hashing function to not account for assertions during update process

--- server/util/aria.js
+++ server/util/aria.js
@@ -1,5 +1,5 @@
 const objectHash = require('object-hash');
-const { omit } = require('lodash');
+const { omit, pick } = require('lodash');
 
 const evaluateAtNameKey = atName => {
     // Could probably add back support for AT keys from the database level
@@ -7,25 +7,52 @@ const evaluateAtNameKey = atName => {
     else return atName.toLowerCase();
 };
 
-const testWithOmittedAttributes = test => ({
-    ...omit(test, [
+const assertionWithModifiedAttributes = (assertion, { forUpdateCompare }) => {
+    // During comparison for phase update, we only need to make sure the assertionId hasn't
+    // changed, so the result isn't affected by the text changing
+    // if (forUpdateCompare) return omit(assertion, ['assertionId']);
+    if (forUpdateCompare) return pick(assertion, ['assertionId']);
+    return omit(assertion, ['id']);
+};
+
+const testWithModifiedAttributes = (test, { forUpdateCompare }) => {
+    let propertiesToOmit = [
         'id',
         'at',
         'renderedUrls',
         'renderedUrl',
         'renderableContent.target.at.raw',
         'viewers'
-    ]),
-    assertions: test.assertions.map(assertion => omit(assertion, ['id'])),
-    scenarios: test.scenarios.map(scenario => omit(scenario, ['id']))
-});
+    ];
+
+    if (forUpdateCompare) {
+        // Changed text in renderableContent.assertions[].assertion(Statement|Phrase) shouldn't matter
+        // during comparison of results
+        propertiesToOmit.push('renderableContent.assertions');
+
+        // The collection of assertions won't matter during a update comparison; a per assertion
+        // check will
+        propertiesToOmit.push('assertions');
+
+        return {
+            ...omit(test, propertiesToOmit),
+            scenarios: test.scenarios.map(scenario => omit(scenario, ['id']))
+        };
+    } else {
+        return {
+            ...omit(test, propertiesToOmit),
+            assertions: test.assertions.map(assertionWithModifiedAttributes),
+            scenarios: test.scenarios.map(scenario => omit(scenario, ['id']))
+        };
+    }
+};
 
 // Ideally the hash of tests being imported will never change
-const hashTests = tests => objectHash(tests.map(testWithOmittedAttributes));
+const hashTests = tests => objectHash(tests.map(testWithModifiedAttributes));
 
 // Generate the hash of a test.
-const hashTest = test => {
-    return objectHash(testWithOmittedAttributes(test));
+const hashTest = (test, { forUpdateCompare = false } = {}) => {
+    return objectHash(testWithModifiedAttributes(test, { forUpdateCompare }));
 };
 
 module.exports = {
