bcd55e7df3d5ec26ec5f22e8692fb58875bc58af
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-03-13 09:40:48 -0500

    Add migration to change assertion.priority

--- server/migrations/20240311192910-removeRequiredAndOptionalPriority.js
+++ server/migrations/20240311192910-removeRequiredAndOptionalPriority.js
@@ -4,27 +4,85 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
         return queryInterface.sequelize.transaction(async transaction => {
-            const testPlanVersions = await queryInterface.sequelize.query(
-                `SELECT id, tests FROM "TestPlanVersion"`,
-                {
-                    type: Sequelize.QueryTypes.SELECT,
-                    transaction
-                }
-            );
-            await Promise.all(
-                testPlanVersions.map(async ({ id, tests }) => {
-                    const updatedTests = JSON.stringify(
-                        tests.map(test => {
-                            delete test.atMode;
-                            return test;
-                        })
-                    );
-                    await queryInterface.sequelize.query(
-                        `UPDATE "TestPlanVersion" SET tests = ? WHERE id = ?`,
-                        { replacements: [updatedTests, id], transaction }
-                    );
-                })
-            );
+            const modifyPrioritiesInTable = async () => {
+                const testPlanVersions = await queryInterface.sequelize.query(
+                    // This query is returning tests that have any assertions that
+                    // have a priority value of REQUIRED or OPTIONAL.
+                    `
+                  SELECT
+                    id,
+                    tests
+                  FROM
+                    "TestPlanVersion"
+                  WHERE
+                    EXISTS (
+                      SELECT
+                        1
+                      FROM
+                        jsonb_array_elements(tests) AS test,
+                        jsonb_array_elements(test -> 'assertions') AS assertion
+                      WHERE
+                        assertion ->> 'priority' = 'REQUIRED'
+                        OR assertion ->> 'priority' = 'OPTIONAL'
+                  )
+                `,
+                    {
+                        type: Sequelize.QueryTypes.SELECT,
+                        transaction
+                    }
+                );
+
+                await Promise.all(
+                    testPlanVersions.map(async ({ id, tests }) => {
+                        const updatedTests = JSON.stringify(
+                            tests.map(test => {
+                                return {
+                                    ...test,
+                                    assertions: test.assertions.map(
+                                        assertion => {
+                                            if (
+                                                assertion.priority ===
+                                                'REQUIRED'
+                                            ) {
+                                                assertion.priority = 'MUST';
+                                            } else if (
+                                                assertion.priority ===
+                                                'OPTIONAL'
+                                            ) {
+                                                assertion.priority = 'SHOULD';
+                                            }
+
+                                            return {
+                                                ...assertion
+                                            };
+                                        }
+                                    )
+                                };
+                            })
+                        );
+                        await queryInterface.sequelize.query(
+                            `UPDATE
+                            "TestPlanVersion"
+                          SET
+                            tests = ?
+                          WHERE
+                            EXISTS (
+                              SELECT
+                                1
+                              FROM
+                                jsonb_array_elements(tests) AS test,
+                                jsonb_array_elements(test -> 'assertions') AS assertion
+                              WHERE
+                                assertion ->> 'priority' = 'REQUIRED'
+                                OR assertion ->> 'priority' = 'OPTIONAL'
+                            )`,
+                            { replacements: [updatedTests, id], transaction }
+                        );
+                    })
+                );
+            };
+            await modifyPrioritiesInTable();
         });
-    }
+    },
+    down: async () => {}
 };
