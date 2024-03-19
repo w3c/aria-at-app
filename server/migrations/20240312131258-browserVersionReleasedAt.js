9d3e5361f0f41f248cab7878a4fa0c7959747436
alflennik <alflennik@users.noreply.github.com>
2024-03-19 17:36:44 -0400

    Add releasedAt to browserVersion

---
+++ server/migrations/20240312131258-browserVersionReleasedAt.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        return queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.addColumn(
+                'BrowserVersion',
+                'releasedAt',
+                {
+                    type: Sequelize.DataTypes.DATE,
+                    allowNull: true
+                },
+                { transaction }
+            );
+        });
+    },
+
+    async down(queryInterface /* , Sequelize */) {
+        return queryInterface.sequelize.transaction(async transaction => {
+            await queryInterface.removeColumn('AtVersion', 'releasedAt', {
+                transaction
+            });
+        });
+    }
+};
