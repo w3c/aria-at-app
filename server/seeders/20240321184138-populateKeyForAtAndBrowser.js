a77cf670b98eba9828adb6c58662e6188436c696
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-03-21 16:26:03 -0500

    Add column to At and Browser tables

---
+++ server/seeders/20240321184138-populateKeyForAtAndBrowser.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface) {
+        await queryInterface.sequelize.query(`
+          UPDATE
+            "At"
+          SET
+            "key" = CASE
+              WHEN id = 1 THEN 'jaws'
+              WHEN id = 2 THEN 'nvda'
+              WHEN id = 3 THEN 'voiceover_macos'
+              ELSE null
+            END;
+        `);
+
+        await queryInterface.sequelize.query(`
+          UPDATE
+            "Browser"
+          SET
+            "key" = CASE
+              WHEN id = 1 THEN 'firefox'
+              WHEN id = 2 THEN 'chrome'
+              WHEN id = 3 THEN 'safari_macos'
+              ELSE null
+            END;
+        `);
+    },
+
+    async down(queryInterface) {
+        await queryInterface.removeColumn('At', 'key');
+        await queryInterface.removeColumn('Browser', 'key');
+    }
+};
