9695e4f460fe5b1e514c7e793c4eac0dc46ca9e8
alflennik <alflennik@users.noreply.github.com>
2024-04-03 17:56:50 -0400

    Prevent API from creating duplicate reports

--- server/resolvers/createTestPlanReportResolver.js
+++ server/resolvers/createTestPlanReportResolver.js
@@ -1,11 +1,32 @@
 const { AuthenticationError } = require('apollo-server-errors');
 const {
-    createTestPlanReport
+    createTestPlanReport,
+    getTestPlanReports
 } = require('../models/services/TestPlanReportService');
 const populateData = require('../services/PopulatedData/populateData');
 
 const createTestPlanReportResolver = async (_, { input }, context) => {
     const { user, transaction } = context;
+
+    // TODO: remove code related to conflictingReports which is meant to be
+    // temporary until the frontend fully supports duplicate AT / Browsers
+    // for TestPlanReports.
+    // START TEMPORARY CODE
+    const conflictingReports = await getTestPlanReports({
+        where: {
+            testPlanVersionId: input.testPlanVersionId,
+            atId: input.atId,
+            browserId: input.browserId
+        },
+        transaction
+    });
+    if (conflictingReports.length) {
+        throw new Error(
+            'Temporarily, creating duplicate reports for a AT / browser ' +
+                'combination is not allowed'
+        );
+    }
+    // END TEMPORARY CODE
 
     if (!user?.roles.find(role => role.name === 'ADMIN')) {
         throw new AuthenticationError();
