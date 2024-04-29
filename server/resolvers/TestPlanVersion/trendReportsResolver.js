7891a4c390f7349da1c9d6bb46f958f52ba4711b
Howard Edwards <howarde.edwards@gmail.com>
2024-04-29 14:44:12 -0500

    WIP: Add example

--- server/resolvers/TestPlanVersion/trendReportsResolver.js
+++ server/resolvers/TestPlanVersion/trendReportsResolver.js
@@ -10,6 +10,22 @@ const trendReportsResolver = async (
     { atId, browserId },
     context
 ) => {
+    // eg. query {
+    //   testPlanVersion(id: 56291) {
+    //     id
+    //     title
+    //     phase
+    //     recommendedPhaseReachedAt
+    //     trendReports(atId: 3, browserId: 3) {
+    //       id
+    //       recommendedAtVersion {
+    //         id
+    //         name
+    //       }
+    //       metrics
+    //     }
+    //   }
+    // }
     const { transaction } = context;
 
     if (testPlanVersion.phase !== 'RECOMMENDED') return null;
