95e8cff286622b06e3c8df80d95eaaa0f8a7615d
Howard Edwards <howarde.edwards@gmail.com>
2024-06-05 12:08:38 -0400

    Update migration to address edge cases

--- server/migrations/20240522032230-fixIncorrectDeprecationDates.js
+++ server/migrations/20240522032230-fixIncorrectDeprecationDates.js
@@ -2,109 +2,116 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface) {
+    async up(queryInterface, Sequelize) {
+        const SECOND = 1000;
+        const MINUTE = 60 * SECOND;
+
+        const versionAndRangeCheck = async (
+            deprecatedVersion,
+            maybeDeprecatingVersion,
+            transaction
+        ) => {
+            let relevantPhaseDateFound = false;
+            let maybeDeprecatingVersionPhaseDate =
+                maybeDeprecatingVersion.updatedAt;
+
+            if (deprecatedVersion.recommendedPhaseReachedAt) {
+                if (!maybeDeprecatingVersion.recommendedPhaseReachedAt) return;
+                maybeDeprecatingVersionPhaseDate =
+                    maybeDeprecatingVersion.recommendedPhaseReachedAt;
+                relevantPhaseDateFound = true;
+            }
+
+            if (
+                deprecatedVersion.candidatePhaseReachedAt &&
+                !relevantPhaseDateFound
+            ) {
+                if (!maybeDeprecatingVersion.candidatePhaseReachedAt) return;
+                maybeDeprecatingVersionPhaseDate =
+                    maybeDeprecatingVersion.candidatePhaseReachedAt;
+                relevantPhaseDateFound = true;
+            }
+
+            if (
+                deprecatedVersion.draftPhaseReachedAt &&
+                !relevantPhaseDateFound
+            ) {
+                if (!maybeDeprecatingVersion.draftPhaseReachedAt) return;
+                maybeDeprecatingVersionPhaseDate =
+                    maybeDeprecatingVersion.draftPhaseReachedAt;
+            }
+
+            // Get a 5-minute range around deprecated version to compare to other
+            // versions which could have potentially deprecated it based on
+            // their phase change dates
+            const deprecatedAtDate = new Date(deprecatedVersion.deprecatedAt);
+            const startDate = new Date(deprecatedAtDate.getTime() - 5 * MINUTE);
+            const endDate = new Date(deprecatedAtDate.getTime() + 5 * MINUTE);
+
+            if (
+                maybeDeprecatingVersionPhaseDate.getTime() >=
+                    startDate.getTime() &&
+                maybeDeprecatingVersionPhaseDate.getTime() <= endDate.getTime()
+            ) {
+                // Set updated deprecatedAt date as being 2 seconds before
+                // whichever dated phase change caused it; exactly how
+                // updatePhaseResolver and import-tests is done
+                const updatedDeprecatedAt = new Date(
+                    new Date(
+                        maybeDeprecatingVersionPhaseDate.getTime() - 2 * SECOND
+                    )
+                );
+
+                await queryInterface.sequelize.query(
+                    `update "TestPlanVersion"
+                         set "deprecatedAt" = ?
+                         where id = ?
+                           and "gitSha" = ?
+                           and directory = ?`,
+                    {
+                        replacements: [
+                            updatedDeprecatedAt,
+                            deprecatedVersion.id,
+                            deprecatedVersion.gitSha,
+                            deprecatedVersion.directory
+                        ],
+                        transaction
+                    }
+                );
+            }
+        };
+
         return queryInterface.sequelize.transaction(async transaction => {
-            const deprecatedRecords = await queryInterface.sequelize.query(
-                `
-                  SELECT
-                    id,
-                    phase,
-                    "deprecatedAt",
-                    "gitSha",
-                    directory
-                  FROM
-                    "TestPlanVersion"
-                  WHERE
-                    "deprecatedAt" IS NOT NULL
-                `,
-                { type: queryInterface.sequelize.QueryTypes.SELECT },
-                {
-                    transaction
-                }
+            const testPlanVersions = await queryInterface.sequelize.query(
+                `select "id",
+                        "phase",
+                        "gitSha",
+                        "directory",
+                        "updatedAt",
+                        "draftPhaseReachedAt",
+                        "candidatePhaseReachedAt",
+                        "recommendedPhaseReachedAt",
+                        "deprecatedAt"
+                from "TestPlanVersion"`,
+                { type: Sequelize.QueryTypes.SELECT, transaction }
             );
 
-            const versionsInRange = await queryInterface.sequelize.query(
-                `
-                  SELECT
-                    id,
-                    "draftPhaseReachedAt",
-                    "candidatePhaseReachedAt",
-                    "recommendedPhaseReachedAt",
-                    "updatedAt",
-                    "gitSha",
-                    directory
-                  FROM
-                    "TestPlanVersion"
-                `,
-                { type: queryInterface.sequelize.QueryTypes.SELECT },
-                {
-                    transaction
-                }
+            const deprecatedVersions = testPlanVersions.filter(
+                ({ phase }) => phase === 'DEPRECATED'
             );
 
-            const versionAndRangeCheck = async (
-                record,
-                version,
-                startRange,
-                endRange
-            ) => {
-                const versionPhaseDates = [
-                    version.updatedAt,
-                    version.draftPhaseReachedAt,
-                    version.candidatePhaseReachedAt,
-                    version.recommendedPhaseReachedAt
-                ];
-
-                for (let versionPhaseDate of versionPhaseDates) {
-                    if (
-                        record.draftPhaseReachedAt === null &&
-                        versionPhaseDate !== version.updatedAt
-                    ) {
-                        continue;
-                    }
-                    if (
-                        versionPhaseDate &&
-                        version.id !== record.id &&
-                        version.directory === record.directory &&
-                        versionPhaseDate >= startRange &&
-                        versionPhaseDate <= endRange
-                    ) {
-                        const newDeprecatedAt = new Date(
-                            new Date(versionPhaseDate.getTime() - 2000)
-                        );
-
-                        await queryInterface.sequelize.query(
-                            `
-                              update "TestPlanVersion"
-                              set "deprecatedAt" = '${newDeprecatedAt.toISOString()}'
-                              where id = ${record.id}
-                              and "gitSha" = '${record.gitSha}'
-                              and directory = '${record.directory}'
-                            `,
-                            {
-                                transaction
-                            }
-                        );
-                    }
-                }
-            };
-
-            for (let record of deprecatedRecords) {
-                const deprecatedAtDate = new Date(record.deprecatedAt);
-
-                const startRange = new Date(
-                    deprecatedAtDate.getTime() - 5 * 60000
-                );
-                const endRange = new Date(
-                    deprecatedAtDate.getTime() + 5 * 60000
+            for (let deprecatedVersion of deprecatedVersions) {
+                const maybeDeprecatingVersions = testPlanVersions.filter(
+                    ({ id, directory }) =>
+                        id !== deprecatedVersion.id &&
+                        directory === deprecatedVersion.directory
                 );
 
-                for (let version of versionsInRange) {
+                for (let maybeDeprecatingVersion of maybeDeprecatingVersions) {
                     await versionAndRangeCheck(
-                        record,
-                        version,
-                        startRange,
-                        endRange
+                        deprecatedVersion,
+                        maybeDeprecatingVersion,
+                        transaction
                     );
                 }
             }
