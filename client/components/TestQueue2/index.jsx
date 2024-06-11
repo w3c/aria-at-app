bd026af92e5db04ee6e063f70be2d8f7e4c9b9df
Howard Edwards <howarde.edwards@gmail.com>
2024-06-11 14:24:19 -0400

    Address #1103

--- client/components/TestQueue2/index.jsx
+++ client/components/TestQueue2/index.jsx
@@ -253,7 +253,7 @@ const TestQueue = () => {
                 )}
                 expanded={testPlan.testPlanVersions.map(
                     testPlanVersion =>
-                        openDisclosuresRef.current[testPlanVersion.id]
+                        openDisclosuresRef.current[testPlanVersion.id] || false
                 )}
                 disclosureContainerView={testPlan.testPlanVersions.map(
                     testPlanVersion =>
