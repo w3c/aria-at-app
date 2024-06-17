fa88f207df9056b965b8ee3554c0ac9a6ba7e031
Howard Edwards <howarde.edwards@gmail.com>
2024-06-17 15:08:13 -0400

    Update AtVersions e2e test

--- client/tests/AtVersions.test.js
+++ client/tests/AtVersions.test.js
@@ -3,7 +3,7 @@ import getPage from './util/getPage';
 describe('AT Version UI', () => {
     test('should add, edit, then remove an AtVersion', async () => {
         /* prettier-ignore */
-        await getPage({ role: 'admin', url: '/test-queue-old' }, async page => {
+        await getPage({ role: 'admin', url: '/test-queue' }, async page => {
             const openTrayIfClosed = async () => {
                 await page.waitForSelector('button ::-p-text(Manage Assistive Technology Versions)');
                 const isTrayClosed = !!(await page.$('::-p-text(Select an assistive technology and manage its versions)'));
@@ -21,7 +21,7 @@ describe('AT Version UI', () => {
             await page.type('.modal-body .form-group:nth-child(2) input', '01-01-2000');
             await page.click('.modal-footer button ::-p-text(Add Version)');
             await page.waitForNetworkIdle();
-            await openTrayIfClosed();
+            await page.click('.modal-footer button ::-p-text(Ok)');
             await page.waitForSelector('.at-versions-container option:nth-child(2) ::-p-text(99.0.1)');
             const optionValue = await page.$eval('.at-versions-container option:nth-child(2)', option => option.value);
             await page.select('.at-versions-container select', optionValue);
@@ -33,14 +33,14 @@ describe('AT Version UI', () => {
             await page.type('.modal-body .form-group:nth-child(1) input', '99.0.99');
             await page.click('.modal-footer button ::-p-text(Save)');
             await page.waitForNetworkIdle();
-            await openTrayIfClosed();
+            await page.click('.modal-footer button ::-p-text(Ok)');
             await page.waitForSelector('.at-versions-container option ::-p-text(99.0.99)');
             await page.select('.at-versions-container select', optionValue);
             await page.click('.at-versions-container button ::-p-text(Remove)');
             await page.waitForSelector('.modal-title ::-p-text(Remove JAWS Version 99.0.99)');
             await page.click('.modal-footer button ::-p-text(Remove)');
             await page.waitForNetworkIdle();
-            await openTrayIfClosed();
+            await page.click('.modal-footer button ::-p-text(Ok)');
             const option = await page.$('.at-versions-container option ::-p-text(99.0.99)');
             expect(option).toBeNull();
         });
