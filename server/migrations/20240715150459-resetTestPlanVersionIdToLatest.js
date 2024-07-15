edcbc473bb82d9204bf54a5824216755ad9c5d7e
Howard Edwards <howarde.edwards@gmail.com>
2024-07-15 11:13:44 -0400

    Reset TestPlanVersion nextval during import if ids go unused

---
+++ server/migrations/20240715150459-resetTestPlanVersionIdToLatest.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { sequelize } = require('../models');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(async transaction => {
+      const [latestTestPlanVersion] = await queryInterface.sequelize.query(
+        `select id
+         from "TestPlanVersion"
+         order by id desc
+         limit 1`,
+        {
+          type: Sequelize.QueryTypes.SELECT,
+          transaction
+        }
+      );
+
+      await sequelize.query(
+        `SELECT setval(pg_get_serial_sequence('"TestPlanVersion"', 'id'), :currentSequenceValue)`,
+        {
+          replacements: { currentSequenceValue: latestTestPlanVersion.id },
+          transaction
+        }
+      );
+    });
+  },
+
+  async down() {}
+};
