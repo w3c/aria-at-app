8b3fe58f39e27bbf23c6ba87e82c7138d7683d39
Howard Edwards <howarde.edwards@gmail.com>
2024-07-17 19:24:25 -0400

    Update UserSettings/queries.js and update UserSettings tests and fix resolver

--- client/tests/util/getPage.js
+++ client/tests/util/getPage.js
@@ -168,6 +168,15 @@ const getPage = async (options, callback) => {
     await page.waitForSelector('::-p-text(Signed in)');
   }
 
+  // Re-attempt If the url wasn't meant to be accessed by a certain role and was
+  // initially redirected away from proper link; eg. 'false' role accessing
+  // /account/settings
+  const currentUrl = await page.url();
+  if (currentUrl.includes('/404') || currentUrl.includes('/invalid-request')) {
+    await page.goto(`${baseUrl}${url}`);
+    await page.waitForNetworkIdle();
+  }
+
   try {
     await callback(page, { baseUrl });
   } finally {
