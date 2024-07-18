79a53a5de774ec749c594e4484a11f38ae6f1cb7
Stalgia Grigg <stalgia@bocoup.com>
2024-07-18 16:26:55 -0700

    Break test parser up

--- server/scripts/import-tests/testParser.js
+++ server/scripts/import-tests/testParser.js
@@ -17,6 +17,31 @@ const parseTests = ({
   isV2
 }) => {
   const tests = [];
+  const { renderedUrlsById, allCollectedById } =
+    collectTestData(builtDirectoryPath);
+
+  Object.entries(allCollectedById).forEach(([rawTestId, allCollected]) => {
+    const renderedUrls = renderedUrlsById[rawTestId];
+
+    validateCollectedData(allCollected);
+
+    createTestsForFormat({
+      allCollected,
+      rawTestId,
+      renderedUrls,
+      isV2,
+      testPlanVersionId,
+      ats,
+      gitSha,
+      builtDirectoryPath,
+      tests
+    });
+  });
+
+  return tests;
+};
+
+const collectTestData = builtDirectoryPath => {
   const renderedUrlsById = {};
   const allCollectedById = {};
 
@@ -26,6 +51,7 @@ const parseTests = ({
     const jsonString = fse.readFileSync(jsonPath, 'utf8');
     const collected = JSON.parse(jsonString);
     const renderedUrl = filePath.replace(/\.json$/, '.html');
+
     if (!allCollectedById[collected.info.testId]) {
       allCollectedById[collected.info.testId] = [];
       renderedUrlsById[collected.info.testId] = [];
@@ -34,217 +60,259 @@ const parseTests = ({
     renderedUrlsById[collected.info.testId].push(renderedUrl);
   });
 
-  /**
-   * Creates tests to be included in [TestPlanVersion.tests]
-   * @param {object} allCollected Generated tests coming from aria-at through test-{number}-{at}-collected.json files.
-   * @param {string|number} rawTestId Numeric id for tests in v1 format, string id for tests in v2 format. Comes directly from `tests.csv` files.
-   * @param {string[]} renderedUrls Paths to .collected.json files.
-   * @param {boolean} isV2 Boolean check to see if the testFormatVersion of the collected tests are for v2, otherwise for v1.
-   */
-  const createTestsForFormat = ({
-    allCollected,
-    rawTestId,
+  return { renderedUrlsById, allCollectedById };
+};
+
+const validateCollectedData = allCollected => {
+  if (
+    !deepPickEqual(allCollected, { excludeKeys: ['at', 'mode', 'commands'] })
+  ) {
+    throw new Error(
+      'Difference found in a part of a .collected.json file which should be equivalent'
+    );
+  }
+};
+
+const createTestsForFormat = ({
+  allCollected,
+  rawTestId,
+  renderedUrls,
+  isV2,
+  testPlanVersionId,
+  ats,
+  gitSha,
+  builtDirectoryPath,
+  tests
+}) => {
+  const getRenderedUrl = createRenderedUrlGetter(
     renderedUrls,
-    // There MAY be a need to handle additional versions. This may be changed to handle that
-    // when that time comes
-    isV2
-  }) => {
-    const getRenderedUrl = index =>
-      getAppUrl(renderedUrls[index], {
-        gitSha,
-        directoryPath: builtDirectoryPath
+    gitSha,
+    builtDirectoryPath
+  );
+
+  if (!isV2) {
+    createV1Test({
+      allCollected,
+      rawTestId,
+      renderedUrls,
+      testPlanVersionId,
+      ats,
+      getRenderedUrl,
+      tests
+    });
+  } else {
+    createV2Tests({
+      allCollected,
+      rawTestId,
+      testPlanVersionId,
+      ats,
+      getRenderedUrl,
+      tests
+    });
+  }
+};
+
+const createRenderedUrlGetter = (renderedUrls, gitSha, builtDirectoryPath) => {
+  return index =>
+    getAppUrl(renderedUrls[index], {
+      gitSha,
+      directoryPath: builtDirectoryPath
+    });
+};
+
+const createV1Test = ({
+  allCollected,
+  rawTestId,
+  testPlanVersionId,
+  ats,
+  getRenderedUrl,
+  tests
+}) => {
+  const common = allCollected[0];
+  const testId = createTestId(testPlanVersionId, common.info.testId);
+  const atIds = allCollected.map(
+    collected => ats.find(at => at.name === collected.target.at.name).id
+  );
+
+  tests.push({
+    id: testId,
+    rowNumber: rawTestId,
+    title: common.info.title,
+    atIds,
+    renderableContent: createRenderableContent(allCollected, atIds),
+    renderedUrls: createRenderedUrls(atIds, getRenderedUrl),
+    scenarios: createScenarios(allCollected, testId, ats),
+    assertions: getAssertions(common, testId),
+    viewers: [],
+    testFormatVersion: 1
+  });
+};
+
+const createV2Tests = ({
+  allCollected,
+  rawTestId,
+  testPlanVersionId,
+  ats,
+  getRenderedUrl,
+  tests
+}) => {
+  allCollected.forEach((collected, collectedIndex) => {
+    const testId = createTestId(
+      testPlanVersionId,
+      `${collected.target.at.key}:${collected.info.testId}`
+    );
+    const atId = ats.find(at => at.name === collected.target.at.name).id;
+
+    tests.push({
+      id: testId,
+      rawTestId,
+      rowNumber: collected.info.presentationNumber,
+      title: collected.info.title,
+      at: createAtObject(collected),
+      atIds: [atId],
+      renderableContent: createV2RenderableContent(collected),
+      renderedUrl: getRenderedUrl(collectedIndex),
+      scenarios: createV2Scenarios(collected, testId, atId),
+      assertions: getAssertions(collected, testId),
+      viewers: [],
+      testFormatVersion: 2
+    });
+  });
+};
+
+const createRenderableContent = (allCollected, atIds) => {
+  return Object.fromEntries(
+    allCollected.map((collected, index) => [atIds[index], collected])
+  );
+};
+
+const createRenderedUrls = (atIds, getRenderedUrl) => {
+  return Object.fromEntries(
+    atIds.map((atId, index) => [atId, getRenderedUrl(index)])
+  );
+};
+
+const createScenarios = (allCollected, testId, ats) => {
+  const scenarios = [];
+  allCollected.forEach(collected => {
+    collected.commands.forEach(command => {
+      scenarios.push({
+        id: createScenarioId(testId, scenarios.length),
+        atId: ats.find(at => at.name === collected.target.at.name).id,
+        commandIds: command.keypresses.map(({ id }) => id)
       });
+    });
+  });
+  return scenarios;
+};
 
-    const getAssertions = (data, testId) => {
-      return data.assertions.map((assertion, index) => {
-        let priority = '';
-        if (assertion.priority === 1) priority = 'MUST';
-        if (assertion.priority === 2) priority = 'SHOULD';
-        // Available for v2
-        if (assertion.priority === 3) priority = 'MAY';
-        if (assertion.priority === 0) priority = 'EXCLUDE';
+const createAtObject = collected => {
+  return {
+    key: collected.target.at.key,
+    name: collected.target.at.name,
+    settings: collected.target.at.raw.settings
+  };
+};
 
-        let result = {
-          id: createAssertionId(testId, index),
-          priority
+const createV2RenderableContent = collected => {
+  return {
+    ...collected,
+    target: {
+      at: collected.target.at,
+      referencePage: collected.target.referencePage,
+      setupScript: collected.target.setupScript
+    },
+    assertions: collected.assertions.map(
+      ({
+        assertionId,
+        priority,
+        assertionStatement,
+        assertionPhrase,
+        refIds,
+        tokenizedAssertionStatements,
+        tokenizedAssertionPhrases
+      }) => {
+        const tokenizedAssertionStatement =
+          tokenizedAssertionStatements[collected.target.at.key];
+        const tokenizedAssertionPhrase =
+          tokenizedAssertionPhrases?.[collected.target.at.key];
+
+        return {
+          assertionId,
+          priority,
+          assertionStatement: tokenizedAssertionStatement || assertionStatement,
+          assertionPhrase: tokenizedAssertionPhrase || assertionPhrase,
+          refIds
         };
+      }
+    )
+  };
+};
 
-        // Available for v1
-        if (assertion.expectation) result.text = assertion.expectation;
+const createV2Scenarios = (collected, testId, atId) => {
+  return collected.commands.map((command, index) => ({
+    id: createScenarioId(testId, `${index}:${command.settings}`),
+    atId,
+    commandIds: command.keypresses.map(({ id }) => id),
+    settings: command.settings
+  }));
+};
 
-        // Available for v2
-        if (assertion.assertionStatement) {
-          const tokenizedAssertionStatement =
-            assertion?.tokenizedAssertionStatements[data.target.at.key];
-          const tokenizedAssertionPhrase =
-            assertion?.tokenizedAssertionPhrases?.[data.target.at.key];
-
-          result.rawAssertionId = assertion.assertionId;
-          result.assertionStatement =
-            tokenizedAssertionStatement || assertion.assertionStatement;
-          result.assertionPhrase =
-            tokenizedAssertionPhrase || assertion.assertionPhrase;
-          result.assertionExceptions = data.commands.flatMap(command => {
-            return command.assertionExceptions
-              .filter(
-                exception => exception.assertionId === assertion.assertionId
-              )
-              .map(({ priority: assertionPriority }) => {
-                let priority = convertAssertionPriority(assertionPriority);
-
-                return {
-                  priority,
-                  commandId: command.id,
-                  settings: command.settings
-                };
-              });
-          });
-        }
-
-        return result;
-      });
+const getAssertions = (data, testId) => {
+  return data.assertions.map((assertion, index) => {
+    const priority = convertPriority(assertion.priority);
+    let result = {
+      id: createAssertionId(testId, index),
+      priority
     };
 
-    // Using the v1 test format, https://github.com/w3c/aria-at/wiki/Test-Format-V1-Definition
-    if (!isV2) {
-      const common = allCollected[0];
-      const testId = createTestId(testPlanVersionId, common.info.testId);
-      const atIds = allCollected.map(
-        collected => ats.find(at => at.name === collected.target.at.name).id
-      );
+    if (assertion.expectation) result.text = assertion.expectation;
 
-      tests.push({
-        id: testId,
-        rowNumber: rawTestId,
-        title: common.info.title,
-        atIds,
-        renderableContent: Object.fromEntries(
-          allCollected.map((collected, index) => {
-            /** @type RenderableContent **/
-            return [atIds[index], collected];
-          })
-        ),
-        renderedUrls: Object.fromEntries(
-          atIds.map((atId, index) => {
-            return [atId, getRenderedUrl(index)];
-          })
-        ),
-        scenarios: (() => {
-          const scenarios = [];
-          allCollected.forEach(collected => {
-            collected.commands.forEach(command => {
-              scenarios.push({
-                id: createScenarioId(testId, scenarios.length),
-                atId: ats.find(at => at.name === collected.target.at.name).id,
-                commandIds: command.keypresses.map(({ id }) => id)
-              });
-            });
-          });
-          return scenarios;
-        })(),
-        assertions: getAssertions(common, testId),
-        viewers: [],
-        testFormatVersion: 1
-      });
+    if (assertion.assertionStatement) {
+      result = {
+        ...result,
+        ...createV2AssertionData(assertion, data)
+      };
     }
 
-    // Using the v2 test format, https://github.com/w3c/aria-at/wiki/Test-Format-Definition-V2
-    if (isV2) {
-      for (const [collectedIndex, collected] of allCollected.entries()) {
-        const testId = createTestId(
-          testPlanVersionId,
-          `${collected.target.at.key}:${collected.info.testId}`
-        );
-
-        let test = {
-          id: testId,
-          rawTestId,
-          rowNumber: collected.info.presentationNumber,
-          title: collected.info.title,
-          at: {
-            key: collected.target.at.key,
-            name: collected.target.at.name,
-            settings: collected.target.at.raw.settings
-          },
-          atIds: [ats.find(at => at.name === collected.target.at.name).id],
-          /** @type RenderableContent **/
-          renderableContent: {
-            ...collected,
-            target: {
-              at: collected.target.at,
-              referencePage: collected.target.referencePage,
-              setupScript: collected.target.setupScript
-            },
-            assertions: collected.assertions.map(
-              ({
-                assertionId,
-                priority,
-                assertionStatement,
-                assertionPhrase,
-                refIds,
-                tokenizedAssertionStatements,
-                tokenizedAssertionPhrases
-              }) => {
-                const tokenizedAssertionStatement =
-                  tokenizedAssertionStatements[collected.target.at.key];
-                const tokenizedAssertionPhrase =
-                  tokenizedAssertionPhrases?.[collected.target.at.key];
-
-                return {
-                  assertionId,
-                  priority,
-                  assertionStatement:
-                    tokenizedAssertionStatement || assertionStatement,
-                  assertionPhrase: tokenizedAssertionPhrase || assertionPhrase,
-                  refIds
-                };
-              }
-            )
-          },
-          renderedUrl: getRenderedUrl(collectedIndex),
-          scenarios: (() => {
-            const scenarios = [];
-            collected.commands.forEach(command => {
-              scenarios.push({
-                id: createScenarioId(
-                  testId,
-                  `${scenarios.length}:${command.settings}`
-                ),
-                atId: ats.find(at => at.name === collected.target.at.name).id,
-                commandIds: command.keypresses.map(({ id }) => id),
-                settings: command.settings
-              });
-            });
-            return scenarios;
-          })(),
-          assertions: getAssertions(collected, testId),
-          viewers: [],
-          testFormatVersion: 2
-        };
-
-        tests.push(test);
-      }
-    }
-  };
-
-  Object.entries(allCollectedById).forEach(([rawTestId, allCollected]) => {
-    const renderedUrls = renderedUrlsById[rawTestId];
-
-    if (
-      !deepPickEqual(allCollected, {
-        excludeKeys: ['at', 'mode', 'commands']
-      })
-    ) {
-      throw new Error(
-        'Difference found in a part of a .collected.json file which ' +
-          'should be equivalent'
-      );
-    }
-
-    createTestsForFormat({ allCollected, rawTestId, renderedUrls, isV2 });
+    return result;
   });
+};
 
-  return tests;
+const convertPriority = priority => {
+  const priorities = { 1: 'MUST', 2: 'SHOULD', 3: 'MAY', 0: 'EXCLUDE' };
+  return priorities[priority] || '';
+};
+
+const createV2AssertionData = (assertion, data) => {
+  const {
+    assertionId,
+    assertionStatement,
+    assertionPhrase,
+    tokenizedAssertionStatements,
+    tokenizedAssertionPhrases
+  } = assertion;
+  const atKey = data.target.at.key;
+
+  return {
+    rawAssertionId: assertionId,
+    assertionStatement:
+      tokenizedAssertionStatements?.[atKey] || assertionStatement,
+    assertionPhrase: tokenizedAssertionPhrases?.[atKey] || assertionPhrase,
+    assertionExceptions: createAssertionExceptions(data.commands, assertionId)
+  };
+};
+
+const createAssertionExceptions = (commands, assertionId) => {
+  return commands.flatMap(command => {
+    return command.assertionExceptions
+      .filter(exception => exception.assertionId === assertionId)
+      .map(({ priority }) => ({
+        priority: convertAssertionPriority(priority),
+        commandId: command.id,
+        settings: command.settings
+      }));
+  });
 };
 
 module.exports = {
