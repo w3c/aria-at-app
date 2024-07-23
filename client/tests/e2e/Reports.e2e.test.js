339bee6438f2002ad2156f3c4de001b11894bc15
Howard Edwards <howarde.edwards@gmail.com>
2024-07-23 17:12:56 -0400

    Update Reports tests

--- client/tests/e2e/Reports.e2e.test.js
+++ client/tests/e2e/Reports.e2e.test.js
@@ -119,14 +119,120 @@ describe('Report page for recommended report', () => {
       expect(validVoiceoverHeadingMatchFound).toBe(true);
     });
   });
+
+  it('clicks View Results button and navigates to /targets/:id page', async () => {
+    await getPage({ role: false, url: '/report/67' }, async page => {
+      const viewResultsButton =
+        'a[role="button"] ::-p-text(View Complete Results)';
+
+      await page.waitForSelector(viewResultsButton);
+      await page.click(viewResultsButton);
+
+      await page.waitForSelector(
+        'h1 ::-p-text(Checkbox Example (Mixed-State))'
+      );
+      await page.waitForSelector('h2 ::-p-text(Versions Summary)');
+      await page.waitForSelector('h2 ::-p-text(Results for)');
+      await page.waitForSelector('details summary ::-p-text(Approved Report)');
+      const currentUrl = await page.url();
+
+      expect(currentUrl).toMatch(/^.*\/report\/\d+\/targets\/\d+/);
+    });
+  });
 });
 
 describe('Report page for candidate report', () => {
-  it('renders page h1', async () => {
+  const checkForHeading = async (page, selector, headingText) => {
+    return page.$$eval(
+      selector,
+      (els, headingText) =>
+        Array.from(els)
+          .map(el => el.innerText.trim())
+          .includes(headingText),
+      headingText
+    );
+  };
+
+  it('renders page', async () => {
     await getPage({ role: false, url: '/report/24' }, async page => {
-      await text(page, 'h1 ::-p-text(Modal Dialog Example)');
+      await page.waitForSelector('h1 ::-p-text(Modal Dialog Example)');
+      await page.waitForSelector('h2 ::-p-text(Introduction)');
+      await page.waitForSelector('h2 ::-p-text(Metadata)');
+      await page.waitForSelector(
+        'details summary ::-p-text(Unapproved Report)'
+      );
+      await page.waitForSelector(
+        'a[role="button"] ::-p-text(View Complete Results)'
+      );
+
+      await page.waitForSelector('th ::-p-text(Test Name)');
+      await page.waitForSelector('th ::-p-text(Must-Have Behaviors)');
+      await page.waitForSelector('th ::-p-text(Should-Have Behaviors)');
+      await page.waitForSelector('th ::-p-text(May-Have Behaviors)');
+
+      // Check that the reported date is valid (format is MMM D, YYYY)
+      const reportCompletedOnText = await text(
+        page,
+        '::-p-text(Report completed on)'
+      );
+      let validReportCompletedOnDate = false;
+      const dateFormatRegex =
+        /^(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}$/;
+      const dateText = reportCompletedOnText.split('Report completed on ')[1];
+      if (dateFormatRegex.test(dateText)) {
+        // Confirm if date is valid
+        const date = new Date(dateText);
+        validReportCompletedOnDate = !isNaN(date.getTime());
+      }
+
+      // Check that '<AT> <AtVersion> + <Browser> headings are valid
+      const validJawsHeadingMatchFound = await checkForHeading(
+        page,
+        'h2 ::-p-text(JAWS)',
+        'JAWS and Chrome'
+      );
+      const validNvdaHeadingMatchFound = await checkForHeading(
+        page,
+        'h2 ::-p-text(NVDA)',
+        'NVDA and Chrome'
+      );
+      const validVoiceoverHeadingMatchFound = await checkForHeading(
+        page,
+        'h2 ::-p-text(VoiceOver for macOS)',
+        'VoiceOver for macOS and Safari'
+      );
+
+      expect(validReportCompletedOnDate).toBe(true);
+      expect(validJawsHeadingMatchFound).toBe(true);
+      expect(validNvdaHeadingMatchFound).toBe(true);
+      expect(validVoiceoverHeadingMatchFound).toBe(true);
     });
   });
 
-  // TODO: Additional tests
+  it('clicks View Results button and navigates to /targets/:id page', async () => {
+    await getPage({ role: false, url: '/report/24' }, async page => {
+      const viewResultsButton =
+        'a[role="button"] ::-p-text(View Complete Results)';
+
+      await page.waitForSelector(viewResultsButton);
+      await page.click(viewResultsButton);
+
+      await page.waitForSelector('h1 ::-p-text(Modal Dialog Example)');
+
+      const isVersionsSummaryFound = await page.evaluate(() => {
+        const elementsWithText = Array.from(
+          document.querySelectorAll('h2')
+        ).filter(element => element.textContent.includes('Versions Summary'));
+        return elementsWithText.length > 0;
+      });
+      await page.waitForSelector('h2 ::-p-text(Results for)');
+      await page.waitForSelector(
+        'details summary ::-p-text(Unapproved Report)'
+      );
+      const currentUrl = await page.url();
+
+      expect(isVersionsSummaryFound).toBe(false);
+      expect(currentUrl).toMatch(/^.*\/report\/\d+\/targets\/\d+/);
+    });
+  });
 });
