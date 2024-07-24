33e6883bf5e479e62071efc9ee8fa92f93cee0e3
Stalgia Grigg <stalgia@bocoup.com>
2024-07-24 15:03:48 -0700

    Only test a subset of possible pages

--- client/tests/e2e/scripts/takeSnapshots.js
+++ client/tests/e2e/scripts/takeSnapshots.js
@@ -2,40 +2,13 @@
 const fs = require('fs').promises;
 const path = require('path');
 const prettier = require('prettier');
-const { execSync } = require('child_process');
-
-const {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  HttpLink
-} = require('@apollo/client');
 const getPage = require('../../util/getPage');
 const { setup, teardown } = require('../../util/getPage');
 
-const BASE_URL = 'http://localhost:3033';
 const SNAPSHOTS_DIR = path.join(__dirname, 'snapshots');
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: `${BASE_URL}/api/graphql` }),
-  cache: new InMemoryCache()
-});
-
-function killExistingProcesses() {
-  try {
-    execSync('pkill -f "node.*server/server.js"');
-    execSync('pkill -f "node.*next"');
-  } catch (error) {
-    console.log(
-      'No processes to kill or error killing processes:',
-      error.message
-    );
-  }
-}
 
 async function shutdown() {
   console.log('Shutting down...');
-  killExistingProcesses();
   if (global.browser) {
     await global.browser.close();
   }
@@ -43,7 +16,7 @@ async function shutdown() {
   console.log('Shutdown complete');
 }
 
-async function getBaseRoutes() {
+async function getRoutes() {
   return [
     '/',
     '/signup-instructions',
@@ -52,66 +25,13 @@ async function getBaseRoutes() {
     '/reports',
     '/candidate-review',
     '/data-management',
-    '/404'
-    // TODO
-    // '/candidate-test-plan/:testPlanVersionId/:atId',
+    '/404',
+    '/test-plan-report/1',
+    '/test-review/8',
+    '/run/2',
+    '/data-management/meter',
+    '/candidate-test-plan/24/1'
   ];
-}
-
-function getCandidateTestPlanRoutes() {
-  const atIds = [1, 2, 3];
-  const testPlanVersionId = 24;
-  return atIds.map(atId => `/candidate-test-plan/${testPlanVersionId}/${atId}`);
-}
-
-async function getTestReviewRoutes() {
-  const query = gql`
-    query GetTestPlanVersionIds {
-      testPlanVersions {
-        id
-      }
-    }
-  `;
-  const { data } = await client.query({ query });
-  return data.testPlanVersions.map(({ id }) => `/test-review/${id}`);
-}
-
-// The first 19 are valid
-async function getReportRoutesForTestPlanVersions() {
-  const maxValidTestPlanReportId = 19;
-  return Array.from(
-    { length: maxValidTestPlanReportId },
-    (_, i) => `/test-plan-report/${i + 1}`
-  );
-}
-
-async function getRunRoutes() {
-  const query = gql`
-    query GetRunIds {
-      testPlanRuns {
-        id
-      }
-    }
-  `;
-
-  const { data } = await client.query({ query });
-  return data.testPlanRuns.map(({ id }) => `/run/${id}`);
-}
-
-async function getDataManagementRoutes() {
-  const query = gql`
-    query GetTestPlanDirectories {
-      testPlans {
-        id
-        directory
-      }
-    }
-  `;
-
-  const { data } = await client.query({ query });
-  return data.testPlans.map(
-    ({ directory }) => `/data-management/${directory}/`
-  );
 }
 
 async function takeSnapshot(browser, role, route) {
@@ -151,77 +71,29 @@ async function takeSnapshot(browser, role, route) {
 
     return snapshot;
   } catch (error) {
-    console.error(`Error taking snapshot for ${route}:`, error);
-    throw error;
+    console.warn(`Error taking snapshot for ${route}:`, error);
+    return null;
   }
 }
 
 async function takeSnapshots() {
-  killExistingProcesses();
-
   try {
     const browser = await setup();
     global.browser = browser;
 
-    // const routes = await getBaseRoutes();
+    const routes = await getRoutes();
 
-    // for (const role of ['admin', 'tester', false]) {
-    //   for (const route of routes) {
-    //     const snapshot = await takeSnapshot(browser, role, route);
-    //     const fileName = `${route.replace(/\//g, '_')}_${
-    //       role ? role : ''
-    //     }.html`;
-    //     await fs.writeFile(path.join(SNAPSHOTS_DIR, fileName), snapshot);
-    //     console.log(`Recorded snapshot for ${route} as ${role}`);
-    //   }
-    // }
-
-    // const reportRoutes = await getReportRoutesForTestPlanVersions();
-    // for (const route of reportRoutes) {
-    //   const snapshot = await takeSnapshot(browser, 'admin', route);
-    //   const fileName = `${route.replace(/\//g, '_')}.html`;
-    //   await fs.writeFile(path.join(SNAPSHOTS_DIR, fileName), snapshot);
-    //   console.log(`Recorded snapshot for ${route}`);
-    // }
-
-    // const dataManagementRoutes = await getDataManagementRoutes();
-    // for (const route of dataManagementRoutes) {
-    //   const snapshot = await takeSnapshot(browser, 'admin', route);
-    //   const fileName = `${route.replace(/\//g, '_')}.html`;
-    //   await fs.writeFile(path.join(SNAPSHOTS_DIR, fileName), snapshot);
-    //   console.log(`Recorded snapshot for ${route}`);
-    // }
-
-    // const runRoutes = await getRunRoutes();
-    // for (const route of runRoutes) {
-    //   const snapshot = await takeSnapshot(browser, 'admin', route);
-    //   const fileName = `${route.replace(/\//g, '_')}.html`;
-    //   await fs.writeFile(path.join(SNAPSHOTS_DIR, fileName), snapshot);
-    //   console.log(`Recorded snapshot for ${route}`);
-    // }
-
-    // const testReviewRoutes = await getTestReviewRoutes();
-    // for (const route of testReviewRoutes) {
-    //   try {
-    //     const snapshot = await takeSnapshot(browser, 'admin', route);
-    //     const fileName = `${route.replace(/\//g, '_')}.html`;
-    //     await fs.writeFile(path.join(SNAPSHOTS_DIR, fileName), snapshot);
-    //     console.log(`Recorded snapshot for ${route}`);
-    //   } catch (error) {
-    //     console.warn(`Failed to take snapshot for ${route}`, error);
-    //     continue;
-    //   }
-    // }
-
-    const candidateTestPlanRoutes = getCandidateTestPlanRoutes();
-    for (const route of candidateTestPlanRoutes) {
+    for (const route of routes) {
       const snapshot = await takeSnapshot(browser, 'admin', route);
+      if (!snapshot) {
+        continue;
+      }
       const fileName = `${route.replace(/\//g, '_')}.html`;
       await fs.writeFile(path.join(SNAPSHOTS_DIR, fileName), snapshot);
       console.log(`Recorded snapshot for ${route}`);
     }
   } catch (e) {
-    console.error('Error during snapshot taking:', e);
+    console.warn('Error during snapshot taking:', e);
   } finally {
     await shutdown();
   }
