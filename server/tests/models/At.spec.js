52f8835175a86afdac1fb4c81f45cd33486f54fd
Stalgia Grigg <stalgia@bocoup.com>
2024-09-04 09:51:42 -0700

    Add and update spec tests for Vendor table

--- server/tests/models/At.spec.js
+++ server/tests/models/At.spec.js
@@ -8,6 +8,7 @@ const {
 const AtModel = require('../../models/At');
 const AtVersionModel = require('../../models/AtVersion');
 const BrowserModel = require('../../models/Browser');
+const VendorModel = require('../../models/Vendor');
 
 describe('AtModel', () => {
   // A1
@@ -26,11 +27,13 @@ describe('AtModel', () => {
     // A1
     const AT_VERSION_ASSOCIATION = { as: 'atVersions' };
     const BROWSER_ASSOCIATION = { through: 'AtBrowsers', as: 'browsers' };
+    const VENDOR_ASSOCIATION = { as: 'vendor' };
 
     // A2
     beforeAll(() => {
       Model.hasMany(AtVersionModel, AT_VERSION_ASSOCIATION);
       Model.hasMany(BrowserModel, BROWSER_ASSOCIATION);
+      Model.belongsTo(VendorModel, VENDOR_ASSOCIATION);
     });
 
     it('defined a hasMany association with AtVersion', () => {
@@ -38,6 +41,13 @@ describe('AtModel', () => {
       expect(Model.hasMany).toHaveBeenCalledWith(
         AtVersionModel,
         expect.objectContaining(Model.AT_VERSION_ASSOCIATION)
+      );
+    });
+
+    it('defined a belongsTo association with Vendor', () => {
+      expect(Model.belongsTo).toHaveBeenCalledWith(
+        VendorModel,
+        expect.objectContaining(Model.VENDOR_ASSOCIATION)
       );
     });
   });
