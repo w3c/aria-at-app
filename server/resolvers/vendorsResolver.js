21a1cbcb2fbcc9742d68884835d3f31de5b7e4d0
Stalgia Grigg <stalgia@bocoup.com>
2024-09-03 17:40:59 -0700

    Correct vendors resolver

--- server/resolvers/vendorsResolver.js
+++ server/resolvers/vendorsResolver.js
@@ -1,12 +1,8 @@
 const { findAllVendors } = require('../models/services/VendorService');
 
-const vendorsResolver = async (_, __, { user, transaction }) => {
-  if (!user || !user.roles.some(role => role.name === 'ADMIN')) {
-    throw new Error('Unauthorized');
-  }
+const vendorsResolver = async (_, __, { transaction }) => {
+  // No auth since the vendors.txt file is public
   return findAllVendors(transaction);
 };
 
-module.exports = {
-  vendorsResolver
-};
+module.exports = vendorsResolver;
