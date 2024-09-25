051e41be2438352c0350dfaefc76dc896024fe7f
Stalgia Grigg <stalgia@bocoup.com>
2024-09-25 12:00:17 -0700

    Trim vendor company name

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -83,7 +83,7 @@ const oauthRedirectFromGithubController = async (req, res) => {
       vendor => vendor.split('|')[0] === githubUsername
     );
     if (vendorEntry) {
-      const [, companyName] = vendorEntry.split('|');
+      const [, companyName] = vendorEntry.split('|').trim();
       const vendor = await findVendorByName({
         name: companyName,
         transaction: req.transaction
