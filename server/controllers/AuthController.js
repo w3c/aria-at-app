8f522edab36fe115035cd238e45472d66b832c34
Stalgia Grigg <stalgia@bocoup.com>
2024-10-16 12:02:34 -0700

    Update server/controllers/AuthController.js
    Co-authored-by: jugglinmike <mike@mikepennisi.com>

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -83,7 +83,7 @@ const oauthRedirectFromGithubController = async (req, res) => {
       vendor => vendor.split('|')[0] === githubUsername
     );
     if (vendorEntry) {
-      const [, companyName] = vendorEntry.trim().split('|').map(item => item.trim());
+      const [, companyName] = vendorEntry.trim().split(/\s*\|\s*/);
       const vendor = await findVendorByName({
         name: companyName,
         transaction: req.transaction
