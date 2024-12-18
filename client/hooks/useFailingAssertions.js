a42c7d77e6514138a8233ef22fb1b0b6a7189aec
Stalgia Grigg <stalgia@bocoup.com>
2024-12-17 17:50:54 -0800

    Add e2e tests for summary view in CandidateTestPlanRun

--- client/hooks/useFailingAssertions.js
+++ client/hooks/useFailingAssertions.js
@@ -9,19 +9,25 @@ export const useFailingAssertions = testPlanReport => {
     return testPlanReport.finalizedTestResults.flatMap(
       (testResult, testIndex) => {
         return testResult.scenarioResults.flatMap(scenarioResult => {
-          return scenarioResult.assertionResults
-            .filter(assertionResult => !assertionResult.passed)
-            .map(assertionResult => ({
-              testResultId: testResult.id,
-              testIndex,
-              testTitle: testResult.test.title,
-              scenarioCommands: scenarioResult.scenario.commands
-                .map(cmd => cmd.text)
-                .join(', '),
-              assertionText: assertionResult.assertion.text,
-              priority: assertionResult.assertion.priority,
-              output: scenarioResult.output
-            }));
+          return (
+            scenarioResult.assertionResults
+              .filter(assertionResult => !assertionResult.passed)
+              // We only want to show MUST and SHOULD assertions
+              .filter(
+                assertionResult => assertionResult.assertion.priority !== 'MAY'
+              )
+              .map(assertionResult => ({
+                testResultId: testResult.id,
+                testIndex,
+                testTitle: testResult.test.title,
+                scenarioCommands: scenarioResult.scenario.commands
+                  .map(cmd => cmd.text)
+                  .join(', '),
+                assertionText: assertionResult.assertion.text,
+                priority: assertionResult.assertion.priority,
+                output: scenarioResult.output
+              }))
+          );
         });
       }
     );
