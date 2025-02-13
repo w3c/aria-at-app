d3c4e0207c03f06b14dbb05bfe88e88134d92f06
Stalgia Grigg <stalgia@bocoup.com>
2025-02-13 11:31:52 -0800

    Remove unnecessary version validation in scheduleCollectionJobResolver

--- server/resolvers/scheduleCollectionJobResolver.js
+++ server/resolvers/scheduleCollectionJobResolver.js
@@ -2,12 +2,6 @@ const { AuthenticationError } = require('apollo-server-core');
 const {
   scheduleCollectionJob
 } = require('../models/services/CollectionJobService');
-const {
-  getTestPlanReportById
-} = require('../models/services/TestPlanReportService');
-const {
-  getAtVersionWithRequirements
-} = require('../models/services/AtVersionService');
 
 const scheduleCollectionJobResolver = async (
   _parent,
@@ -20,27 +14,7 @@ const scheduleCollectionJobResolver = async (
     throw new AuthenticationError();
   }
 
-  const report = await getTestPlanReportById({
-    id: testPlanReportId,
-    transaction
-  });
-
-  if (!report) {
-    throw new Error(`Test plan report with id ${testPlanReportId} not found`);
-  }
-
-  // Get the appropriate AT version for automation
-  const atVersion = await getAtVersionWithRequirements(
-    report.at.id,
-    report.exactAtVersion,
-    report.minimumAtVersion,
-    transaction
-  );
-
-  return scheduleCollectionJob(
-    { testPlanReportId, atVersion },
-    { transaction }
-  );
+  return scheduleCollectionJob({ testPlanReportId }, { transaction });
 };
 
 module.exports = scheduleCollectionJobResolver;
