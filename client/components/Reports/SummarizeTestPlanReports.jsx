4c608f678af020cfb60a9bf7727f16aa2467a93c
Howard Edwards <howarde.edwards@gmail.com>
2025-03-06 15:41:48 -0500

    Updated SummarizeTestPlanReports with a new module

--- client/components/Reports/SummarizeTestPlanReports.jsx
+++ client/components/Reports/SummarizeTestPlanReports.jsx
@@ -3,46 +3,31 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { Container, Table } from 'react-bootstrap';
-import styled from '@emotion/styled';
+import clsx from 'clsx';
 import alphabetizeObjectBy from '../../utils/alphabetizeObjectBy';
 import { derivePhaseName } from '../../utils/aria';
 import { none } from './None';
 import { getTestPlanTargetTitle, getTestPlanVersionTitle } from './getTitles';
 import ClippedProgressBar from '@components/common/ClippedProgressBar';
 import { TestPlanVersionPropType } from '../common/proptypes';
-
-const FullHeightContainer = styled(Container)`
-  min-height: calc(100vh - 64px);
-`;
-
-const PhaseText = styled.span`
-  font-size: 12px;
-  margin-left: 6px;
-  padding: 4px 6px;
-  border-radius: 12px;
-  overflow: hidden;
-  white-space: nowrap;
-  color: white;
-
-  &.candidate {
-    background: #f87f1b;
-  }
-
-  &.recommended {
-    background: #b253f8;
-  }
-`;
+import commonStyles from '../common.module.css';
+import styles from './SummarizeTestPlanReports.module.css';
 
 const SummarizeTestPlanReports = ({ testPlanVersions }) => {
   if (!testPlanVersions.length) {
     return (
-      <FullHeightContainer id="main" as="main" tabIndex="-1">
+      <Container
+        id="main"
+        as="main"
+        tabIndex="-1"
+        className={commonStyles.fhContainer}
+      >
         <Helmet>
           <title>AT Interop Reports | ARIA-AT</title>
         </Helmet>
         <h1>Assistive Technology Interoperability Reports</h1>
         <p>There are no results to show just yet. Please check back soon!</p>
-      </FullHeightContainer>
+      </Container>
     );
   }
 
@@ -62,7 +47,12 @@ const SummarizeTestPlanReports = ({ testPlanVersions }) => {
   );
 
   return (
-    <FullHeightContainer id="main" as="main" tabIndex="-1">
+    <Container
+      id="main"
+      as="main"
+      tabIndex="-1"
+      className={commonStyles.fhContainer}
+    >
       <Helmet>
         <title>AT Interop Reports | ARIA-AT</title>
       </Helmet>
@@ -107,9 +97,15 @@ const SummarizeTestPlanReports = ({ testPlanVersions }) => {
                     >
                       {getTestPlanVersionTitle(testPlanVersion)}
                     </Link>
-                    <PhaseText className={phase.toLowerCase()} aria-hidden>
+                    <span
+                      className={clsx(
+                        styles.phaseText,
+                        styles[phase.toLowerCase()]
+                      )}
+                      aria-hidden
+                    >
                       {derivePhaseName(phase)}
-                    </PhaseText>
+                    </span>
                   </td>
                   {Object.values(testPlanTargetsById).map(testPlanTarget => {
                     const testPlanReport = testPlanReports.find(
@@ -147,7 +143,7 @@ const SummarizeTestPlanReports = ({ testPlanVersions }) => {
             })}
         </tbody>
       </Table>
-    </FullHeightContainer>
+    </Container>
   );
 };
 
