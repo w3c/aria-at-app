f3a8aa26773de1086f9cc0a75d7ccbb498718a13
Stalgia Grigg <stalgia@bocoup.com>
2025-04-17 14:09:10 -0700

    Better loading states for reset button, reload page upon confirm

--- client/components/common/ResetDbButton/index.jsx
+++ client/components/common/ResetDbButton/index.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useTriggerLoad } from '../LoadingStatus';
 import { useThemedModal, THEMES } from '../../../hooks/useThemedModal';
+import LoadingStatus from '../LoadingStatus/LoadingStatus';
 
 const ResetDbButton = () => {
-  const { triggerLoad, loadingMessage } = useTriggerLoad();
+  const { triggerLoad, loadingMessage, loadingNote } = useTriggerLoad();
   const {
     themedModal,
     showThemedModal,
     setShowThemedModal,
     setThemedModalTitle,
     setThemedModalContent,
-    setThemedModalType
+    setThemedModalType,
+    setThemedModalActions,
+    hideThemedModal
   } = useThemedModal({
     type: THEMES.SUCCESS,
     title: 'Success'
   });
+
+  const handleConfirmReload = () => {
+    window.location.reload();
+  };
 
   const handleResetDb = async () => {
     await triggerLoad(
@@ -32,7 +39,18 @@ const ResetDbButton = () => {
           }
           setThemedModalType(THEMES.SUCCESS);
           setThemedModalTitle('Success');
-          setThemedModalContent(<>The database has been reset successfully.</>);
+          setThemedModalContent(
+            <>
+              The database has been reset successfully. Click OK to reload the
+              page.
+            </>
+          );
+          setThemedModalActions([
+            {
+              text: 'OK',
+              action: handleConfirmReload
+            }
+          ]);
           setShowThemedModal(true);
         } catch (e) {
           setThemedModalType(THEMES.DANGER);
@@ -42,6 +60,12 @@ const ResetDbButton = () => {
               {e.message || 'An error occurred while resetting the database'}
             </div>
           );
+          setThemedModalActions([
+            {
+              text: 'OK',
+              action: hideThemedModal
+            }
+          ]);
           setShowThemedModal(true);
         }
       },
@@ -50,18 +74,11 @@ const ResetDbButton = () => {
     );
   };
 
-  if (process.env.ENVIRONMENT !== 'sandbox') {
-    return null;
-  }
-
   return (
     <>
-      <Button
-        variant="danger"
-        onClick={handleResetDb}
-        disabled={!!loadingMessage}
-      >
-        {loadingMessage ? loadingMessage : 'Reset DB'}
+      <LoadingStatus message={loadingMessage} note={loadingNote} />
+      <Button variant="danger" onClick={handleResetDb}>
+        Reset DB
       </Button>
       {showThemedModal && themedModal}
     </>
