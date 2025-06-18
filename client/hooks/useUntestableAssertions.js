a42f4273c9e1ad8b429ce3f36556abc910db3dba
Mike Pennisi <mike@mikepennisi.com>
2025-06-18 19:31:54 -0400

    Add dedicated table for untestable assertions
    > when untestable assertions are present, after the Summary of Negative
    > Side Effects and before the results for the first test, add a section
    > with:
    >
    > 1. A heading with content: "Summary of Untestable Assertions (N must,
    >    N should, N may)"
    > 2. A table with the same columns and type of content as the summary of
    >    failing assertions:
    >    1. Test Name
    >    2. Command
    >    3. Assertion Priority
    >    4. Assertion
    >    5. AT_NAME Response
    > 3. The table should have an accessible name provided by the heading.

---
+++ client/hooks/useUntestableAssertions.js
@@ -0,0 +1,62 @@
+import { useMemo } from 'react';
+
+export const useUntestableAssertions = testPlanReport => {
+  return useMemo(() => {
+    if (!testPlanReport?.finalizedTestResults) {
+      return [];
+    }
+
+    return testPlanReport.finalizedTestResults.flatMap(
+      (testResult, testIndex) => {
+        return testResult.scenarioResults.flatMap(scenarioResult => {
+          const commonResult = {
+            testResultId: testResult.id,
+            testIndex,
+            testTitle: testResult.test.title,
+            scenarioCommands: scenarioResult.scenario.commands
+              .map((cmd, index) => {
+                if (index === scenarioResult.scenario.commands.length - 1) {
+                  return cmd.text;
+                }
+                // Prevent instances of duplicated setting in brackets.
+                // eg. Down Arrow (virtual cursor active) then Down Arrow (virtual cursor active)
+                //
+                // Expectation is Down Arrow then Down Arrow (virtual cursor active), because the setting will always be
+                // the same for the listed key combination.
+                //
+                // Some revision of how that key combination + setting is rendered may be useful
+                return cmd.text.split(' (')[0];
+              })
+              .join(' then '),
+            commandId: `${
+              scenarioResult.scenario.id
+            }_${scenarioResult.scenario.commands.map(cmd => cmd.id).join('_')}`
+          };
+
+          /**
+           * @param {'MUST'|'SHOULD'|'MAY'} priority
+           * @return {object[]}
+           */
+          const processPriorityAssertionResults = priority => {
+            return scenarioResult[
+              `${priority.toLowerCase()}AssertionResults`
+            ].map(assertionResult => ({
+              ...commonResult,
+              priority,
+              assertionText: assertionResult.assertion.text,
+              output: scenarioResult.output
+            }));
+          };
+
+          return !scenarioResult.untestable
+            ? []
+            : [
+                ...processPriorityAssertionResults('MUST'),
+                ...processPriorityAssertionResults('SHOULD'),
+                ...processPriorityAssertionResults('MAY')
+              ];
+        });
+      }
+    );
+  }, [testPlanReport]);
+};
