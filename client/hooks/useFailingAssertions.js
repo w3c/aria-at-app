097907a98a978420061992824934cd514af57903
Mike Pennisi <mike@mikepennisi.com>
2025-06-18 19:59:45 -0400

    Reduce duplication

--- client/hooks/useFailingAssertions.js
+++ client/hooks/useFailingAssertions.js
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import summarizeScenario from '../utils/summarizeScenario';
 
 export const useFailingAssertions = testPlanReport => {
   return useMemo(() => {
@@ -9,29 +10,11 @@ export const useFailingAssertions = testPlanReport => {
     const failingAssertions = testPlanReport.finalizedTestResults.flatMap(
       (testResult, testIndex) => {
         return testResult.scenarioResults.flatMap(scenarioResult => {
-          const commonResult = {
-            testResultId: testResult.id,
+          const commonResult = summarizeScenario(
+            testResult,
             testIndex,
-            testTitle: testResult.test.title,
-            scenarioCommands: scenarioResult.scenario.commands
-              .map((cmd, index) => {
-                if (index === scenarioResult.scenario.commands.length - 1) {
-                  return cmd.text;
-                }
-                // Prevent instances of duplicated setting in brackets.
-                // eg. Down Arrow (virtual cursor active) then Down Arrow (virtual cursor active)
-                //
-                // Expectation is Down Arrow then Down Arrow (virtual cursor active), because the setting will always be
-                // the same for the listed key combination.
-                //
-                // Some revision of how that key combination + setting is rendered may be useful
-                return cmd.text.split(' (')[0];
-              })
-              .join(' then '),
-            commandId: `${
-              scenarioResult.scenario.id
-            }_${scenarioResult.scenario.commands.map(cmd => cmd.id).join('_')}`
-          };
+            scenarioResult
+          );
 
           /**
            * @param {'MUST'|'SHOULD'} priority
