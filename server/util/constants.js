fc4b299c0518f141cbaa975244a278d90f6973c4
Howard Edwards <howarde.edwards@gmail.com>
2025-07-10 19:19:52 -0400

    feat: Add support for VoiceOver on macOS 15 with VoiceOver Bot (#1456)
    * adds macos-15 version for collection jobs
    
    * fix atVersion regression in CollectionJobService
    
    * chore: update GH Workflow config docs
    
    * Specially handle which workflow is selected when running VoiceOver Bot on macOS 15 (#1455)
    
    * Update GithubWorkflowService.js to support calling self-hosted-macos-15.yml
    
    * Update docs on separate workflow to support macos_version=15
    
    * Simplify function for getting the workflow yml for macos
    
    ---------
    
    Co-authored-by: Chris Cuellar <58723+ChrisC@users.noreply.github.com>

--- server/util/constants.js
+++ server/util/constants.js
@@ -4,7 +4,9 @@ const NO_OUTPUT_STRING = 'No output was detected.';
 const AT_VERSIONS_SUPPORTED_BY_COLLECTION_JOBS = {
   // These are tracked with the available versions in the github workflow file
   // https://github.com/bocoup/aria-at-gh-actions-helper/blob/main/.github/workflows/voiceover-test.yml#L39
-  'VoiceOver for macOS': ['13.0', '14.0'],
+  // Separate workflow exists for supporting macOS 15 on self-hosted runner
+  // https://github.com/bocoup/aria-at-gh-actions-helper/blob/main/.github/workflows/self-hosted-macos-15.yml
+  'VoiceOver for macOS': ['13.0', '14.0', '15.0'],
   // These are tracked with the https://github.com/bocoup/aria-at-automation-nvda-builds/releases
   NVDA: ['2024.4.1', '2024.1', '2023.3.3', '2023.3'],
   JAWS: ['2025.2506.168.400']
