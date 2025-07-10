349aebbdec40bed716f40cd83721a2c289d6b455
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2025-07-10 10:12:22 -0700

    adds macos-15 version for collection jobs

--- server/util/constants.js
+++ server/util/constants.js
@@ -4,7 +4,7 @@ const NO_OUTPUT_STRING = 'No output was detected.';
 const AT_VERSIONS_SUPPORTED_BY_COLLECTION_JOBS = {
   // These are tracked with the available versions in the github workflow file
   // https://github.com/bocoup/aria-at-gh-actions-helper/blob/main/.github/workflows/voiceover-test.yml#L39
-  'VoiceOver for macOS': ['13.0', '14.0'],
+  'VoiceOver for macOS': ['13.0', '14.0', '15.0'],
   // These are tracked with the https://github.com/bocoup/aria-at-automation-nvda-builds/releases
   NVDA: ['2024.4.1', '2024.1', '2023.3.3', '2023.3']
 };
