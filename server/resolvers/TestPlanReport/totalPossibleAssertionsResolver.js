9573c13d0a182f69bb86cc311756bd33351f6a69
Stalgia Grigg <stalgia@bocoup.com>
2025-09-17 09:51:03 -0700

    Simple in-memory cache

--- server/resolvers/TestPlanReport/totalPossibleAssertionsResolver.js
+++ server/resolvers/TestPlanReport/totalPossibleAssertionsResolver.js
@@ -2,6 +2,21 @@ const {
   computeTotalPossibleAssertionsForReport
 } = require('../../models/services/TestPlanReportService');
 
+// Simple in-memory cache keyed by TestPlanReport id
+const cache = new Map();
+
 module.exports = testPlanReport => {
-  return computeTotalPossibleAssertionsForReport(testPlanReport) || 0;
+  const reportId = testPlanReport && testPlanReport.id;
+
+  if (reportId != null && cache.has(reportId)) {
+    return cache.get(reportId);
+  }
+
+  const total = computeTotalPossibleAssertionsForReport(testPlanReport) || 0;
+
+  if (reportId != null) {
+    cache.set(reportId, total);
+  }
+
+  return total;
 };
