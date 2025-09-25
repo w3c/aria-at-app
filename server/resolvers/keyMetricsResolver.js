f7616b287a94192b504395cf5da1babe9157a38f
Corey Frang <gnarf37@gmail.com>
2025-09-25 13:57:27 -0400

    update snapshots and mock info for snapshots

--- server/resolvers/keyMetricsResolver.js
+++ server/resolvers/keyMetricsResolver.js
@@ -5,6 +5,16 @@ let cacheTime = 0;
 let cacheExpire = 60 * 60 * 1000;
 
 const keyMetricsResolver = async (_, __, context) => {
+  // mock out the results in the test environment to make snapshots easier to test
+  if (process.env.ENVIRONMENT == 'test') {
+    return {
+      date: 1758821000000,
+      verdictsCount: 12345,
+      commandsCount: 1234,
+      contributorsCount: 12,
+      verdictsLast90Count: 123
+    };
+  }
   if (cacheTime < Date.now() - cacheExpire) {
     cacheTime = Date.now();
     cachedResult = await getKeyMetrics(context);
