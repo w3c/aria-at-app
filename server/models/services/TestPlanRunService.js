24392a08b8c9dfb2ddf9b2b3ddc75ee9fa6f7e0e
Corey Frang <gnarf37@gmail.com>
2025-09-25 13:27:16 -0400

    move query to service

--- server/models/services/TestPlanRunService.js
+++ server/models/services/TestPlanRunService.js
@@ -426,6 +426,30 @@ const getTestResultsUsingAtVersion = async (atVersionId, { transaction }) => {
   return results;
 };
 
+const getKeyMetrics = async ({ transaction }) => {
+  const [[result]] = await sequelize.query(`
+SELECT 
+    (EXTRACT(EPOCH from NOW()) * 1000)::bigint as date,
+    COUNT(assertions->'passed')::int as "verdictsCount",
+    COUNT(DISTINCT scenarios->>'scenarioId')::int as "commandsCount",
+    COUNT(DISTINCT run."testerUserId")::int as "contributorsCount",
+    SUM(CASE WHEN (DATE(results->>'completedAt') >= CURRENT_DATE - INTERVAL '90 days') THEN 1 ELSE 0 END)::int as "verdictsLast90Count"
+FROM 
+    "TestPlanRun" AS run
+    CROSS JOIN LATERAL jsonb_array_elements(run."testResults") AS results
+    CROSS JOIN LATERAL jsonb_array_elements(results->'scenarioResults') AS scenarios
+    CROSS JOIN LATERAL jsonb_array_elements(scenarios->'assertionResults') AS assertions
+    LEFT OUTER JOIN "User" u ON run."testerUserId" = u."id"
+WHERE 
+    assertions->'passed' IS NOT NULL
+    AND assertions->>'passed' <> ''
+    AND NOT u."isBot";
+    `);
+  // convert the date to a real "number"
+  result.date = parseInt(result.date, 10);
+  return result;
+};
+
 module.exports = {
   // TestPlanRun
   getTestPlanRunById,
@@ -437,5 +461,6 @@ module.exports = {
   removeTestPlanRunResultsByQuery,
 
   // Custom functions
-  getTestResultsUsingAtVersion
+  getTestResultsUsingAtVersion,
+  getKeyMetrics
 };
--- server/resolvers/keyMetricsResolver.js
+++ server/resolvers/keyMetricsResolver.js
@@ -1,34 +1,13 @@
+const { getKeyMetrics } = require('../models/services/TestPlanRunService');
+
 let cachedResult = {};
 let cacheTime = 0;
 let cacheExpire = 60 * 60 * 1000;
 
 const keyMetricsResolver = async (_, __, context) => {
   if (cacheTime < Date.now() - cacheExpire) {
-    const {
-      transaction: { sequelize }
-    } = context;
-    const [[result]] = await sequelize.query(`
-SELECT 
-    (EXTRACT(EPOCH from NOW()) * 1000)::bigint as date,
-    COUNT(assertions->'passed')::int as "verdictsCount",
-    COUNT(DISTINCT scenarios->>'scenarioId')::int as "commandsCount",
-    COUNT(DISTINCT run."testerUserId")::int as "contributorsCount",
-    SUM(CASE WHEN (DATE(results->>'completedAt') >= CURRENT_DATE - INTERVAL '90 days') THEN 1 ELSE 0 END)::int as "verdictsLast90Count"
-FROM 
-    "TestPlanRun" AS run
-    CROSS JOIN LATERAL jsonb_array_elements(run."testResults") AS results
-    CROSS JOIN LATERAL jsonb_array_elements(results->'scenarioResults') AS scenarios
-    CROSS JOIN LATERAL jsonb_array_elements(scenarios->'assertionResults') AS assertions
-    LEFT OUTER JOIN "User" u ON run."testerUserId" = u."id"
-WHERE 
-    assertions->'passed' IS NOT NULL
-    AND assertions->>'passed' <> ''
-    AND NOT u."isBot";
-    `);
     cacheTime = Date.now();
-    // convert the date to a real "number"
-    result.date = parseInt(result.date, 10);
-    cachedResult = result;
+    cachedResult = await getKeyMetrics(context);
   }
 
   return cachedResult;
