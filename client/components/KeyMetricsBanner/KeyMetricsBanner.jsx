47d801888864f384a9137cea36d2d209b08c10b5
Mx Corey Frang <corey@bocoup.com>
2025-10-02 13:30:46 -0400

    feat: Add Key Metrics banner to homepage (#1546)
    * draft key mettics banner
    
    * comment out dismissable part of key metrics
    
    * add graphql schema/query
    
    * add graphql query to fetch the data
    
    * actual queries
    
    * remove useState unused
    
    * move query to service
    
    * remove unused var
    
    * update snapshots and mock info for snapshots
    
    * Add tests count to key metrics
    
    * across ___ tests words
    
    * remove commented lines

---
+++ client/components/KeyMetricsBanner/KeyMetricsBanner.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+import { KEY_METRICS_QUERY } from './queries';
+import { useQuery } from '@apollo/client';
+import styles from './KeyMetricsBanner.module.css';
+
+const KeyMetricsBanner = () => {
+  const { data: keyMetricsQuery } = useQuery(KEY_METRICS_QUERY);
+  const {
+    date,
+    verdictsCount,
+    commandsCount,
+    contributorsCount,
+    verdictsLast90Count,
+    testsCount
+  } = keyMetricsQuery?.keyMetrics ?? {};
+  return (
+    <Alert
+      className={styles.keyMetrics}
+      variant="primary"
+      show={keyMetricsQuery}
+    >
+      {keyMetricsQuery && (
+        <>
+          As of <strong>{new Date(date).toDateString().substring(4)}</strong>,{' '}
+          <strong>{verdictsCount.toLocaleString()}</strong> interop verdicts for{' '}
+          <strong>{commandsCount.toLocaleString()}</strong> AT commands across{' '}
+          <strong>{testsCount.toLocaleString()}</strong> tests enabled by{' '}
+          <strong>{contributorsCount.toLocaleString()}</strong> contributors,{' '}
+          <strong>{verdictsLast90Count.toLocaleString()}</strong> verdicts in
+          the last 90 days.
+        </>
+      )}
+    </Alert>
+  );
+};
+
+export default KeyMetricsBanner;
