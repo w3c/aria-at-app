3f6a7a0796b1f8c1a1ff9e409ac1245dad959ad7
Stalgia Grigg <stalgia@bocoup.com>
2025-10-16 13:51:08 -0700

    Remove deprecated const

--- server/util/constants.js
+++ server/util/constants.js
@@ -1,9 +1,6 @@
 // Mirror of constant in /client/components/TestRenderer/OutputTextArea/constants.js
 const NO_OUTPUT_STRING = 'No output was detected.';
 
-// Deprecated: AT versions supported by jobs now stored in DB on AtVersion
-const AT_VERSIONS_SUPPORTED_BY_COLLECTION_JOBS = {};
-
 const VENDOR_NAME_TO_AT_MAPPING = {
   vispero: ['JAWS'],
   nvAccess: ['NVDA'],
@@ -12,6 +9,5 @@ const VENDOR_NAME_TO_AT_MAPPING = {
 
 module.exports = {
   NO_OUTPUT_STRING,
-  AT_VERSIONS_SUPPORTED_BY_COLLECTION_JOBS,
   VENDOR_NAME_TO_AT_MAPPING
 };
