c329b85891ed1dc16033378ad439921d93f63510
Mx Corey Frang <corey@bocoup.com>
2025-10-30 15:46:20 -0400

    Update redirect (thanks stalgia)
    Co-authored-by: Stalgia Grigg <stalgia@bocoup.com>

--- client/routes/index.js
+++ client/routes/index.js
@@ -19,7 +19,7 @@ import TestQueueConflicts from '../components/TestQueue/Conflicts';
 
 export default () => (
   <Routes>
-    <Route index path="/" exact element={<Reports />} />
+    <Route path="/" element={<Navigate to="/reports" replace />} />
     <Route exact path="/about" element={<Home />} />
     <Route exact path="/signup-instructions" element={<SignupInstructions />} />
     <Route
