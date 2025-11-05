31b056f816dcff5563f587a1c20a6622306ea411
Howard Edwards <howarde.edwards@gmail.com>
2025-11-05 15:33:23 -0500

    Fix tests

--- server/tests/integration/graphql.test.js
+++ server/tests/integration/graphql.test.js
@@ -239,7 +239,9 @@ describe('graphql', () => {
               refId
               type
               linkText
+              rawLinkText
               value
+              rawValue
               total
               passed
               failed
@@ -252,7 +254,9 @@ describe('graphql', () => {
               refId
               type
               linkText
+              rawLinkText
               value
+              rawValue
               total
               passed
               failed
@@ -269,7 +273,9 @@ describe('graphql', () => {
               refId
               type
               linkText
+              rawLinkText
               value
+              rawValue
               total
               passed
               failed
@@ -282,7 +288,9 @@ describe('graphql', () => {
               refId
               type
               linkText
+              rawLinkText
               value
+              rawValue
               total
               passed
               failed
@@ -306,7 +314,9 @@ describe('graphql', () => {
               refId
               type
               linkText
+              rawLinkText
               value
+              rawValue
               total
               passed
               failed
