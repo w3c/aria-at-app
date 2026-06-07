cd0f4f9eb7bd82ee2a7708f2713e8dfd00fbc77a
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2026-06-07 12:52:18 -0700

    disable graphql introspection on deployed servers
    also hides debug and error logs

--- server/graphql-server.js
+++ server/graphql-server.js
@@ -1,17 +1,33 @@
 const { ApolloServer } = require('apollo-server-express');
 const {
-  ApolloServerPluginLandingPageGraphQLPlayground
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled
 } = require('apollo-server-core');
 const graphqlSchema = require('./graphql-schema');
 const getGraphQLContext = require('./graphql-context');
 const resolvers = require('./resolvers');
 
+const isDeployed = ['production', 'staging', 'sandbox'].includes(
+  process.env.ENVIRONMENT
+);
+
 const apolloServer = new ApolloServer({
   typeDefs: graphqlSchema,
   context: getGraphQLContext,
   resolvers,
-  // The newer IDE does not work because of CORS issues
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+  introspection: !isDeployed,
+  debug: !isDeployed,
+  formatError: err => {
+    if (isDeployed && err.extensions) {
+      delete err.extensions.exception;
+    }
+    return err;
+  },
+  plugins: [
+    isDeployed
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground()
+  ]
 });
 
 module.exports = apolloServer;
